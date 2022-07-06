4cafa5932fd78ff0452d95f1362aba83cbd5473c
wjmwss <wjmwss@outlook.com>
2022-07-06 11:42:10 +0800

    - feature: 新增十六进制、数组工具；

---
+++ itcp-sugar-tools/src/main/java/cn/srd/itcp/sugar/tools/core/ArraysUtil.java
@@ -0,0 +1,13 @@
+package cn.srd.itcp.sugar.tools.core;
+
+import cn.hutool.core.util.ArrayUtil;
+
+/**
+ * 数组工具
+ *
+ * @author wjm
+ * @date 2022-07-06
+ */
+public class ArraysUtil extends ArrayUtil {
+
+}
