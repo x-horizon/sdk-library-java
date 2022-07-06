e747274775df6f4c36da785105f89c13b4a50958
wjmwss <wjmwss@outlook.com>
2022-07-06 17:51:49 +0800

    - fix: 修复配置类未加入 IOC 容器的问题；

--- itcp-sugar-redisson/src/main/java/cn/srd/itcp/sugar/redisson/support/RedissonProperties.java
+++ itcp-sugar-redisson/src/main/java/cn/srd/itcp/sugar/redisson/support/RedissonProperties.java
@@ -3,9 +3,11 @@ package cn.srd.itcp.sugar.redisson.support;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
+@Component
 @ConfigurationProperties(prefix = "spring.redis.redisson")
 public class RedissonProperties {
 
