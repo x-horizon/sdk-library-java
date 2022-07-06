7068cae76c536bc69c94d59ddf69b53f7420517d
wjmwss <wjmwss@outlook.com>
2022-07-06 17:52:05 +0800

    - style: 优化代码格式；

--- itcp-sugar-redisson/src/main/java/cn/srd/itcp/sugar/redisson/SugarRedissonApplication.java
+++ itcp-sugar-redisson/src/main/java/cn/srd/itcp/sugar/redisson/SugarRedissonApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
 @Import(SpringsUtil.class)
+@SpringBootApplication
 public class SugarRedissonApplication {
 
     public static void main(String[] args) {
