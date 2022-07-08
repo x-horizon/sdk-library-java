cdbc7496e1a1c7f25d45913d088a2461a7c768b6
wjmwss <wjmwss@outlook.com>
2022-07-08 17:26:49 +0800

    - feature: 增加同时拥有所有角色、拥有任一角色的授权注解；

---
+++ itcp-sugar-security/itcp-sugar-security-sa-token/src/main/java/cn/srd/itcp/sugar/security/sa/token/core/SaHasAllRole.java
@@ -0,0 +1,41 @@
+package cn.srd.itcp.sugar.security.sa.token.core;
+
+import cn.dev33.satoken.annotation.SaCheckRole;
+import cn.dev33.satoken.annotation.SaMode;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.lang.annotation.*;
+
+/**
+ * <pre>
+ *   该注解为：同时拥有所有角色时可进入被该注解标记了的方法，如：
+ *
+ *     // 同时拥有 "user1"、"user2" 角色时可进入 test 方法
+ *     &#064;{@link SaHasAllRole}({"user1","user2"})
+ *     &#064;{@link PostMapping}
+ *     public void test(Test test) {
+ *     }
+ *
+ *   可标记在函数上；
+ *   可标记在类上，效果等同于标记在此类的所有方法上；
+ * </pre>
+ *
+ * @author wjm
+ * @date 2022-07-07
+ */
+@SaCheckRole(mode = SaMode.AND)
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface SaHasAllRole {
+
+    /**
+     * 需要校验的权限码，如：@{@link SaHasAllRole}("admin")
+     *
+     * @return 需要校验的角色标识
+     */
+    @AliasFor(annotation = SaCheckRole.class)
+    String[] value() default {};
+
+}
---
+++ itcp-sugar-security/itcp-sugar-security-sa-token/src/main/java/cn/srd/itcp/sugar/security/sa/token/core/SaHasAnyPermission.java
@@ -0,0 +1,41 @@
+package cn.srd.itcp.sugar.security.sa.token.core;
+
+import cn.dev33.satoken.annotation.SaCheckPermission;
+import cn.dev33.satoken.annotation.SaMode;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.lang.annotation.*;
+
+/**
+ * <pre>
+ *   该注解为：拥有任一权限时可进入被该注解标记了的方法，如：
+ *
+ *     // 拥有 "system:user:add"、"system:user:edit" 任一权限时可进入 test 方法
+ *     &#064;{@link SaHasAllPermission}({"system:user:add","system:user:edit"})
+ *     &#064;{@link PostMapping}
+ *     public void test(Test test) {
+ *     }
+ *
+ *   可标记在函数上；
+ *   可标记在类上，效果等同于标记在此类的所有方法上；
+ * </pre>
+ *
+ * @author wjm
+ * @date 2022-07-07
+ */
+@SaCheckPermission(mode = SaMode.OR)
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface SaHasAnyPermission {
+
+    /**
+     * 需要校验的权限码，如：@{@link SaHasAnyPermission}("system:config:add")
+     *
+     * @return 需要校验的权限码
+     */
+    @AliasFor(annotation = SaCheckPermission.class)
+    String[] value() default {};
+
+}
