daae49a9c322b6f2720453d5d89f1466a5a95574
wjmwss <wjmwss@outlook.com>
2022-07-20 00:49:02 +0800

    - feature: 移除无用功能；

--- itcp-sugar-convert/itcp-sugar-convert-jackson/src/main/java/cn/srd/itcp/sugar/convert/jackson/support/SugarConvertJacksonAutoConfiguration.java
+++ itcp-sugar-convert/itcp-sugar-convert-jackson/src/main/java/cn/srd/itcp/sugar/convert/jackson/support/SugarConvertJacksonAutoConfiguration.java
@@ -1,26 +1,26 @@
-package cn.srd.itcp.sugar.convert.jackson.support;
-
-import cn.srd.itcp.sugar.convert.jackson.core.EnableJacksonCapableToEnumDeserializer;
-import cn.srd.itcp.sugar.tools.core.Objects;
-import cn.srd.itcp.sugar.tools.core.SpringsUtil;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-
-/**
- * {@link EnableAutoConfiguration AutoConfiguration} for Sugar Convert Jackson
- *
- * @author wjm
- * @date 2022-07-19 20:25:11
- */
-public class SugarConvertJacksonAutoConfiguration {
-
-    @SuppressWarnings("unchecked")
-    @Bean
-    public <E extends Enum<E>> JacksonCapableToEnumDeserializer<E> jacksonCapableToEnumDeserializer() {
-        if (Objects.isNotEmpty(SpringsUtil.scanPackageByAnnotation(EnableJacksonCapableToEnumDeserializer.class))) {
-            return SpringsUtil.registerCapableBean(JacksonCapableToEnumDeserializer.class);
-        }
-        return null;
-    }
-
-}
+// package cn.srd.itcp.sugar.convert.jackson.support;
+//
+// import cn.srd.itcp.sugar.convert.jackson.core.EnableJacksonCapableToEnumDeserializer;
+// import cn.srd.itcp.sugar.tools.core.Objects;
+// import cn.srd.itcp.sugar.tools.core.SpringsUtil;
+// import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+// import org.springframework.context.annotation.Bean;
+//
+// /**
+//  * {@link EnableAutoConfiguration AutoConfiguration} for Sugar Convert Jackson
+//  *
+//  * @author wjm
+//  * @date 2022-07-19 20:25:11
+//  */
+// public class SugarConvertJacksonAutoConfiguration {
+//
+//     @SuppressWarnings("unchecked")
+//     @Bean
+//     public <E extends Enum<E>> JacksonCapableToEnumDeserializer<E> jacksonCapableToEnumDeserializer() {
+//         if (Objects.isNotEmpty(SpringsUtil.scanPackageByAnnotation(EnableJacksonCapableToEnumDeserializer.class))) {
+//             return SpringsUtil.registerCapableBean(JacksonCapableToEnumDeserializer.class);
+//         }
+//         return null;
+//     }
+//
+// }
