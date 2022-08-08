cf0fc19ea38d36562eed6069eacba3863c747c0e
wjmwss <wjmwss@outlook.com>
2022-08-08 20:25:10 +0800

    - feature: 增加字符集常量；

---
+++ itcp-sugar-tools/src/main/java/cn/srd/itcp/sugar/tools/constant/Charset.java
@@ -0,0 +1,13 @@
+package cn.srd.itcp.sugar.tools.constant;
+
+/**
+ * 字符集
+ *
+ * @author wjm
+ * @since 2022-08-08 17:59:12
+ */
+public class Charset {
+
+    public static final String UTF8 = "utf-8";
+
+}
