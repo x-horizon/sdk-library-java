19218d1e585a96a2775f3750c2beed6123eb1092
xiongjing <1793658353@qq.com>
2022-08-10 08:51:25 +0800

    - feature : 补充提交 数字转二进制注解

---
+++ itcp-sugar-convert/itcp-sugar-convert-mapstruct/src/main/java/cn/srd/itcp/sugar/convert/mapstruct/utils/ParseHighestOneBit.java
@@ -0,0 +1,18 @@
+package cn.srd.itcp.sugar.convert.mapstruct.utils;
+
+import org.mapstruct.Qualifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author
+ * @since
+ */
+@Qualifier
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.CLASS)
+public @interface ParseHighestOneBit {
+}
