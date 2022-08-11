e4bb29a2cdb38f8ea570e2269c447c9b6e2d49e3
wjmwss <wjmwss@outlook.com>
2022-08-11 11:05:50 +0800

    - feature: 增加 Boolean 工具；

---
+++ itcp-sugar-tools/src/main/java/cn/srd/itcp/sugar/tools/core/BooleansUtil.java
@@ -0,0 +1,13 @@
+package cn.srd.itcp.sugar.tools.core;
+
+import cn.hutool.core.util.BooleanUtil;
+
+/**
+ * Boolean 工具
+ *
+ * @author wjm
+ * @since 2022-08-11 10:59:16
+ */
+public class BooleansUtil extends BooleanUtil {
+
+}
