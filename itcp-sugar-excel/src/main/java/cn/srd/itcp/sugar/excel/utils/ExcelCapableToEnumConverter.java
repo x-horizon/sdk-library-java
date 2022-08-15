182ae92cb79fc82e7bc201263b81810ef2d49c73
wjmwss <wjmwss@outlook.com>
2022-08-16 00:17:03 +0800

    - feature: 新增 Excel 属性类型转换器；

---
+++ itcp-sugar-excel/src/main/java/cn/srd/itcp/sugar/excel/utils/ExcelCapableToEnumConverter.java
@@ -0,0 +1,13 @@
+package cn.srd.itcp.sugar.excel.utils;
+
+import com.alibaba.excel.converters.Converter;
+
+/**
+ * Excel 属性类型转换器：Enum 属性值 => Enum
+ *
+ * @author wjm
+ * @since 2022-08-15 23:42:11
+ */
+public class ExcelCapableToEnumConverter implements Converter<String> {
+
+}
