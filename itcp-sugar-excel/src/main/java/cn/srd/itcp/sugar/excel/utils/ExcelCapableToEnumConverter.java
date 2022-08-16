f9a7129561e2fbe1b2d319a92869c947b8af5316
wjmwss <wjmwss@outlook.com>
2022-08-16 22:55:21 +0800

    - feature: 新增 excel 导入时的枚举相关转换器；

--- itcp-sugar-excel/src/main/java/cn/srd/itcp/sugar/excel/utils/ExcelCapableToEnumConverter.java
+++ itcp-sugar-excel/src/main/java/cn/srd/itcp/sugar/excel/utils/ExcelCapableToEnumConverter.java
@@ -1,6 +1,8 @@
 package cn.srd.itcp.sugar.excel.utils;
 
+import cn.srd.itcp.sugar.tools.core.enums.EnumsUtil;
 import com.alibaba.excel.converters.Converter;
+import com.alibaba.excel.converters.ReadConverterContext;
 
 /**
  * Excel 属性类型转换器：Enum 属性值 => Enum
@@ -8,6 +10,12 @@ import com.alibaba.excel.converters.Converter;
  * @author wjm
  * @since 2022-08-15 23:42:11
  */
-public class ExcelCapableToEnumConverter implements Converter<String> {
+public class ExcelCapableToEnumConverter implements Converter<Enum<?>> {
+
+    @SuppressWarnings("all")
+    @Override
+    public Enum<?> convertToJavaData(ReadConverterContext<?> context) {
+        return EnumsUtil.capableToEnum(context.getReadCellData().getStringValue(), (Class<Enum>) context.getContentProperty().getField().getType());
+    }
 
 }
