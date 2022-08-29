40caa7f84ad7e7408c7dfd95a1c3240216814a16
wjmwss <wjmwss@outlook.com>
2022-08-29 11:24:40 +0800

    - feature: 增加扫描子类/实现类、增加自动获取 bean name 注册 bean 功能；

--- itcp-sugar-tools/src/main/java/cn/srd/itcp/sugar/tools/core/SpringsUtil.java
+++ itcp-sugar-tools/src/main/java/cn/srd/itcp/sugar/tools/core/SpringsUtil.java
@@ -90,6 +90,20 @@ public class SpringsUtil extends SpringUtil {
     }
 
     /**
+     * 扫描被 {@link SpringBootApplication} 注解了的启动类包下所有指定类或接口的子类或实现类
+     *
+     * @param superClass 父类或接口
+     * @return 类集合
+     */
+    public static Set<Class<?>> scanPackagesBySuper(final Class<?> superClass) {
+        try {
+            return ClassesUtil.scanPackagesBySuper(new String[]{getRootPackagePath()}, superClass);
+        } catch (Exception ignore) {
+            return CollectionsUtil.newHashSet();
+        }
+    }
+
+    /**
      * 获取 @SpringBootApplication 启动类所在的包路径
      *
      * @return
@@ -116,7 +130,17 @@ public class SpringsUtil extends SpringUtil {
     }
 
     /**
-     * 向 IOC 注册 bean，并返回注册后的 bean
+     * 向 IOC 注册 bean，使用类名首字母小写作为 bean name
+     *
+     * @param clazz 要注册的 bean
+     * @param <T>   要注册的 bean 类型
+     */
+    public static <T> void registerBean(Class<T> clazz) {
+        registerBean(StringsUtil.lowerFirst(clazz.getSimpleName()), clazz);
+    }
+
+    /**
+     * 向 IOC 注册 bean，并返回注册后的 bean，使用类名首字母小写作为 bean name
      * <pre>
      * 该方法支持注册带有特定注解功能的 bean，例如如下 bean 未被 spring 扫描到，使用该方法注册后该 bean 将具备 {@link org.springframework.web.bind.annotation.RestControllerAdvice RestControllerAdvice} 的功能
      * &#064;{@link org.springframework.web.bind.annotation.RestControllerAdvice RestControllerAdvice}
