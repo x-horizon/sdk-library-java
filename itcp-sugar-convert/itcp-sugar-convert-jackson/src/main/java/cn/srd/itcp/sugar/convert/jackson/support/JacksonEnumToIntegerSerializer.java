a30f7c2e7ae2ab13e299fb01cace424883f76702
wjmwss <wjmwss@outlook.com>
2022-09-16 17:18:40 +0800

    - optimize: 优化实现；

--- itcp-sugar-convert/itcp-sugar-convert-jackson/src/main/java/cn/srd/itcp/sugar/convert/jackson/support/JacksonEnumToIntegerSerializer.java
+++ itcp-sugar-convert/itcp-sugar-convert-jackson/src/main/java/cn/srd/itcp/sugar/convert/jackson/support/JacksonEnumToIntegerSerializer.java
@@ -1,8 +1,5 @@
 package cn.srd.itcp.sugar.convert.jackson.support;
 
-import cn.srd.itcp.sugar.convert.jackson.exception.JacksonSerializerException;
-import cn.srd.itcp.sugar.tools.core.Objects;
-import cn.srd.itcp.sugar.tools.core.StringsUtil;
 import cn.srd.itcp.sugar.tools.core.enums.EnumsUtil;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
@@ -19,12 +16,8 @@ public class JacksonEnumToIntegerSerializer extends JsonSerializer<Enum<?>> {
 
     @Override
     @SneakyThrows
-    public void serialize(Enum<?> prepareToDeserializerEnum, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) {
-        Integer prepareToDeserializerValue = EnumsUtil.getEnumValue(prepareToDeserializerEnum, Integer.class);
-        if (Objects.isNull(prepareToDeserializerValue)) {
-            throw new JacksonSerializerException(StringsUtil.format("枚举 “{}” 不包含 Integer 类型的字段，请检查！", prepareToDeserializerEnum.getClass().getName()));
-        }
-        jsonGenerator.writeNumber(prepareToDeserializerValue);
+    public void serialize(Enum<?> prepareToSerializerEnum, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) {
+        jsonGenerator.writeObject(EnumsUtil.getEnumValue(prepareToSerializerEnum, Integer.class));
     }
 
 }
--- itcp-sugar-convert/itcp-sugar-convert-jackson/src/main/java/cn/srd/itcp/sugar/convert/jackson/support/JacksonEnumToLongSerializer.java
+++ itcp-sugar-convert/itcp-sugar-convert-jackson/src/main/java/cn/srd/itcp/sugar/convert/jackson/support/JacksonEnumToLongSerializer.java
@@ -1,8 +1,5 @@
 package cn.srd.itcp.sugar.convert.jackson.support;
 
-import cn.srd.itcp.sugar.convert.jackson.exception.JacksonSerializerException;
-import cn.srd.itcp.sugar.tools.core.Objects;
-import cn.srd.itcp.sugar.tools.core.StringsUtil;
 import cn.srd.itcp.sugar.tools.core.enums.EnumsUtil;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
@@ -19,12 +16,8 @@ public class JacksonEnumToLongSerializer extends JsonSerializer<Enum<?>> {
 
     @Override
     @SneakyThrows
-    public void serialize(Enum<?> prepareToDeserializerEnum, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) {
-        Long prepareToDeserializerValue = EnumsUtil.getEnumValue(prepareToDeserializerEnum, Long.class);
-        if (Objects.isNull(prepareToDeserializerValue)) {
-            throw new JacksonSerializerException(StringsUtil.format("枚举 “{}” 不包含 Long 类型的字段, 无法序列化, 请检查！", prepareToDeserializerEnum.getClass().getName()));
-        }
-        jsonGenerator.writeNumber(prepareToDeserializerValue);
+    public void serialize(Enum<?> prepareToSerializerEnum, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) {
+        jsonGenerator.writeObject(EnumsUtil.getEnumValue(prepareToSerializerEnum, Long.class));
     }
 
 }
