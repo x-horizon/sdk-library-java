59cdc4705407d929af24c979622befeaf4bc7110
wjmwss <wjmwss@outlook.com>
2022-10-13 12:18:35 +0800

    - feature: 增加相关转换工具；

---
+++ itcp-sugar-convert/itcp-sugar-convert-jackson/src/main/java/cn/srd/itcp/sugar/convert/jackson/support/JacksonLongToStringSerializer.java
@@ -0,0 +1,22 @@
+package cn.srd.itcp.sugar.convert.jackson.support;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import lombok.SneakyThrows;
+
+/**
+ * Jackson 序列化处理器：Long => String
+ *
+ * @author wjm
+ * @since 2022-10-13 10:26:34
+ */
+public class JacksonLongToStringSerializer extends JsonSerializer<Long> {
+
+    @Override
+    @SneakyThrows
+    public void serialize(Long prepareToSerializerLong, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) {
+        jsonGenerator.writeObject(String.valueOf(prepareToSerializerLong));
+    }
+
+}
