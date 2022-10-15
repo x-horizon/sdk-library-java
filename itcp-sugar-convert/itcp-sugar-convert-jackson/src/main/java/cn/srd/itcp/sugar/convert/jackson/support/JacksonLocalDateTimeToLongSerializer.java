586242f0da7f53d6c05abef709fc95b5eb576316
wjmwss <wjmwss@outlook.com>
2022-10-15 10:04:30 +0800

    - feature: 支持 localdatetime => long 序列化；

---
+++ itcp-sugar-convert/itcp-sugar-convert-jackson/src/main/java/cn/srd/itcp/sugar/convert/jackson/support/JacksonLocalDateTimeToLongSerializer.java
@@ -0,0 +1,26 @@
+package cn.srd.itcp.sugar.convert.jackson.support;
+
+import cn.srd.itcp.sugar.tools.core.TimeUtil;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import lombok.SneakyThrows;
+
+import java.time.LocalDateTime;
+
+/**
+ * Jackson 序列化处理器：LocalDateTime => Long
+ *
+ * @author wjm
+ * @since 2022-10-14 18:16:31
+ */
+public class JacksonLocalDateTimeToLongSerializer extends JsonSerializer<LocalDateTime> {
+
+    @Override
+    @SneakyThrows
+    public void serialize(LocalDateTime prepareToSerializerLocalDateTime, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) {
+        jsonGenerator.writeObject(TimeUtil.toLong(prepareToSerializerLocalDateTime));
+    }
+
+}
+
