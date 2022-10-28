fcdffebe4cf4a8ae651b333deef6d508b99e23a9
wjmwss <wjmwss@outlook.com>
2022-10-28 11:19:11 +0800

    - feature: 支持 Long => LocalDateTime；

---
+++ itcp-sugar-convert/itcp-sugar-convert-jackson/src/main/java/cn/srd/itcp/sugar/convert/jackson/support/JacksonLongToLocalDateTimeDeserializer.java
@@ -0,0 +1,21 @@
+package cn.srd.itcp.sugar.convert.jackson.support;
+
+import cn.srd.itcp.sugar.tools.core.TimeUtil;
+import com.fasterxml.jackson.databind.util.StdConverter;
+
+import java.time.LocalDateTime;
+
+/**
+ * Jackson 反序列化处理器：Long => LocalDateTime
+ *
+ * @author wjm
+ * @since 2022-10-28 11:18:19
+ */
+public class JacksonLongToLocalDateTimeDeserializer extends StdConverter<Long, LocalDateTime> {
+
+    @Override
+    public LocalDateTime convert(Long value) {
+        return TimeUtil.toLocalDateTime(value);
+    }
+
+}
