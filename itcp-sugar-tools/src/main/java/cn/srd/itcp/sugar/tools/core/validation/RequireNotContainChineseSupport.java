d07d837ebcfbdfc72daf29b821ade1f4856eb064
xiongjing <1793658353@qq.com>
2022-11-02 21:30:25 +0800

    - feature: 新增字符串校验不能包含中文字符；

---
+++ itcp-sugar-tools/src/main/java/cn/srd/itcp/sugar/tools/core/validation/RequireNotContainChineseSupport.java
@@ -0,0 +1,22 @@
+package cn.srd.itcp.sugar.tools.core.validation;
+
+import cn.srd.itcp.sugar.tools.core.StringsUtil;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * 用于验证字符串类型的字段不包含中文字符
+ *
+ * @author
+ * @see RequireNotContainChinese
+ * @since
+ */
+public class RequireNotContainChineseSupport implements ConstraintValidator<RequireNotContainChinese, String> {
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return !StringsUtil.isChineseIncluded(value);
+    }
+
+}
