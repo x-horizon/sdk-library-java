279338101e5456f8d7918526c6e483f7eb9f6815
wjmwss <wjmwss@outlook.com>
2023-01-28 15:06:53 +0800

    - refactor: 全面升级 knife4j；

--- itcp-sugar-tools/src/main/java/cn/srd/itcp/sugar/tools/page/PageParam.java
+++ itcp-sugar-tools/src/main/java/cn/srd/itcp/sugar/tools/page/PageParam.java
@@ -1,7 +1,6 @@
 package cn.srd.itcp.sugar.tools.page;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -17,7 +16,7 @@ import java.io.Serializable;
  * @author wjm
  * @since 2020/01/05 13:45
  */
-@ApiModel(description = "分页参数模型")
+@Schema(description = "分页参数模型")
 @Data
 @Accessors(chain = true)
 public class PageParam implements Serializable {
@@ -28,7 +27,7 @@ public class PageParam implements Serializable {
     /**
      * 页码
      */
-    @ApiModelProperty(value = "页码", required = true, example = "1")
+    @Schema(description = "页码", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     @NotNull(message = "页码不能为空")
     @Min(value = 1, message = "页码最小值为1")
     private Integer pageIndex;
@@ -36,7 +35,7 @@ public class PageParam implements Serializable {
     /**
      * 每页显示条数
      */
-    @ApiModelProperty(value = "每页显示条数", required = true, example = "10")
+    @Schema(description = "每页显示条数", requiredMode = Schema.RequiredMode.REQUIRED, example = "10")
     @NotNull(message = "每页显示条数不能为空")
     @Range(min = 1, max = 100, message = "每页显示条数范围为[1, 100]")
     private Integer pageSize;
--- itcp-sugar-tools/src/main/java/cn/srd/itcp/sugar/tools/page/PageResult.java
+++ itcp-sugar-tools/src/main/java/cn/srd/itcp/sugar/tools/page/PageResult.java
@@ -1,7 +1,6 @@
 package cn.srd.itcp.sugar.tools.page;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -15,7 +14,7 @@ import java.util.List;
  * @author wjm
  * @since 2020/01/05 13:45
  */
-@ApiModel(description = "分页结果模型")
+@Schema(description = "分页结果模型")
 @Data
 @Accessors(chain = true)
 public class PageResult<T> implements Serializable {
@@ -26,37 +25,37 @@ public class PageResult<T> implements Serializable {
     /**
      * 总条数
      */
-    @ApiModelProperty(value = "总条数", required = true, example = "1000")
+    @Schema(description = "总条数", requiredMode = Schema.RequiredMode.REQUIRED, example = "1000")
     private Long total;
 
     /**
      * 总页数
      */
-    @ApiModelProperty(value = "总页数", required = true, example = "100")
+    @Schema(description = "总页数", requiredMode = Schema.RequiredMode.REQUIRED, example = "100")
     private Long totalPages;
 
     /**
      * 当前页码
      */
-    @ApiModelProperty(value = "当前页码", required = true, example = "1")
+    @Schema(description = "当前页码", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
     private Long currentPage;
 
     /**
      * 每页显示条数
      */
-    @ApiModelProperty(value = "每页显示条数", required = true, example = "10")
+    @Schema(description = "每页显示条数", requiredMode = Schema.RequiredMode.REQUIRED, example = "10")
     private Long pageSize;
 
     /**
      * 集合数据
      */
-    @ApiModelProperty(value = "集合数据", required = true)
+    @Schema(description = "集合数据", requiredMode = Schema.RequiredMode.REQUIRED)
     private List<T> data;
 
     /**
      * 对象数据
      */
-    @ApiModelProperty(value = "对象数据", required = false)
+    @Schema(description = "对象数据", required = false)
     private Object datum;
 
 }
