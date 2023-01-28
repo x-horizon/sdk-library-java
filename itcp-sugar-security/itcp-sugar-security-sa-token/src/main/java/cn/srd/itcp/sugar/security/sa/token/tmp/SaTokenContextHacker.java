606bede228f9dfaead1b7652b19330c6eb444ec9
wjmwss <wjmwss@outlook.com>
2023-01-28 15:04:47 +0800

    - fix: 修复 JDK 17 sa-token 使用了 javax 导致的相关问题；

---
+++ itcp-sugar-security/itcp-sugar-security-sa-token/src/main/java/cn/srd/itcp/sugar/security/sa/token/tmp/SaTokenContextHacker.java
@@ -0,0 +1,26 @@
+package cn.srd.itcp.sugar.security.sa.token.tmp;
+
+import cn.dev33.satoken.SaManager;
+import cn.dev33.satoken.context.SaTokenContext;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+/**
+ * TODO wjm 修改 javax 为 Jakarta，暂时用于兼容 JDK 17，后续官方版本支持后去掉
+ *
+ * @author wjm
+ * @since 2023-01-28 00:29:11
+ */
+@Component
+public class SaTokenContextHacker {
+
+    /**
+     * 注入自定义 {@link SaTokenContext}
+     */
+    @PostConstruct
+    public void init() {
+        SaTokenContext saTokenContext = new SaTokenContextForSpring();
+        SaManager.setSaTokenContext(saTokenContext);
+    }
+
+}
