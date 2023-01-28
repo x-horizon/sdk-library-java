63cf3a82e56c53e2628bcdbe30dfe8920e519b50
wjmwss <wjmwss@outlook.com>
2023-01-28 15:06:16 +0800

    - style: 增加启动类；

---
+++ itcp-sugar-excel/src/main/java/cn/srd/itcp/sugar/excel/SugarExcelApplication.java
@@ -0,0 +1,21 @@
+package cn.srd.itcp.sugar.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * boot
+ */
+@SpringBootApplication
+public class SugarExcelApplication {
+
+    /**
+     * main
+     *
+     * @param args main argument
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SugarExcelApplication.class, args);
+    }
+
+}
