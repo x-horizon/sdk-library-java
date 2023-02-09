24bfe1ead132bb241279bcc32bed4362c99b15ec
wjmwss <wjmwss@outlook.com>
2023-02-09 11:44:12 +0800

    - style: 优化注释；

--- itcp-sugar-jdk/itcp-sugar-jdk-hack/src/main/java/cn/itcp/sugar/jdk/hack/core/EnableExportAllModuleWithJDK16.java
+++ itcp-sugar-jdk/itcp-sugar-jdk-hack/src/main/java/cn/itcp/sugar/jdk/hack/core/EnableExportAllModuleWithJDK16.java
@@ -5,7 +5,7 @@ import java.lang.annotation.*;
 /**
  * 启用导出所有模块
  * <pre>
- *     由于 JDK9、JDK16 的模块化改动导致的相关错误，如：module java.base does not "opens java.lang" to unnamed module 等待
+ *     由于 JDK9、JDK16 的模块化改动导致的相关错误，如：module java.base does not "opens java.lang" to unnamed module 等等；
  *     使用该注解来导出所有模块，参考链接如下：
  *     <a href="https://dev.to/jjbrt/how-to-avoid-resorting-to-add-exports-and-add-opens-in-jdk-16-and-later-j3m">Exporting all modules to all modules at runtime on Java 16 and later</a>
  *     <a href="https://github.com/burningwave/core">BurningWave GitHub</a>
