34c434f53c186d4aed8542321714973e23fcb53e
wjmwss <wjmwss@outlook.com>
2023-02-09 11:24:44 +0800

    - feature: 支持 jdk hack 相关功能 - 模块化下禁止访问模块 - 强制导出所有模块；

---
+++ itcp-sugar-jdk/itcp-sugar-jdk-hack/src/main/java/cn/itcp/sugar/jdk/hack/SugarJDKHackApplication.java
@@ -0,0 +1,21 @@
+package cn.itcp.sugar.jdk.hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * boot
+ */
+@SpringBootApplication
+public class SugarJDKHackApplication {
+
+    /**
+     * main
+     *
+     * @param args main argument
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SugarJDKHackApplication.class, args);
+    }
+
+}
