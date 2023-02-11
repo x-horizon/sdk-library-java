c50630f252ae7a471b07f161a592f8c44df82f0e
wjmwss <wjmwss@outlook.com>
2023-02-11 17:44:42 +0800

    - feature: docker api 初步实现；

---
+++ itcp-sugar-component/itcp-sugar-component-docker/src/main/java/cn/srd/itcp/sugar/component/docker/SugarDockerApplication.java
@@ -0,0 +1,21 @@
+package cn.srd.itcp.sugar.component.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * boot
+ */
+@SpringBootApplication
+public class SugarDockerApplication {
+
+    /**
+     * main
+     *
+     * @param args main argument
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SugarDockerApplication.class, args);
+    }
+
+}
