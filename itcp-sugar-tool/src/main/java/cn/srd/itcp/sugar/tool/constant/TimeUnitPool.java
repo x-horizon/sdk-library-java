e559bcc42ce16b1e2db48dc1a41c84bdc537f441
wjmwss <wjmwss@outlook.com>
2023-02-14 15:45:46 +0800

    - feature: 新增时间相关常量；

--- itcp-sugar-tool/src/main/java/cn/srd/itcp/sugar/tool/constant/TimeUnitPool.java
+++ itcp-sugar-tool/src/main/java/cn/srd/itcp/sugar/tool/constant/TimeUnitPool.java
@@ -16,45 +16,66 @@ public enum TimeUnitPool {
     /**
      * millisecond
      */
-    MILLISECOND("ms", "MS", "millisecond", "Millisecond", "MILLISECOND", "毫秒"),
+    MILLISECOND("ms", "MS", "millisecond", "Millisecond", "MILLISECOND", "毫秒", "毫秒钟"),
 
     /**
      * second
      */
-    SECOND("s", "S", "second", "Second", "SECOND", "秒"),
+    SECOND("s", "S", "second", "Second", "SECOND", "秒", "秒钟"),
 
     /**
      * minute
      */
-    MINUTE("m", "M", "minute", "Minute", "MINUTE", "分钟"),
+    MINUTE("m", "M", "minute", "Minute", "MINUTE", "分", "分钟"),
 
     /**
      * hour
      */
-    HOUR("h", "H", "hour", "Hour", "HOUR", "小时"),
+    HOUR("h", "H", "hour", "Hour", "HOUR", "时", "小时"),
 
     /**
      * day
      */
-    DAY("d", "D", "day", "Day", "DAY", "天"),
+    DAY("d", "D", "day", "Day", "DAY", "天", "天数"),
 
     /**
      * month
      */
-    MONTH("m", "M", "month", "Month", "MONTH", "月"),
+    MONTH("m", "M", "month", "Month", "MONTH", "月", "月数"),
 
     /**
      * year
      */
-    YEAR("y", "Y", "year", "Year", "YEAR", "年"),
+    YEAR("y", "Y", "year", "Year", "YEAR", "年", "年数"),
 
     ;
 
-    private final String lowerCaseShortenedName;
-    private final String upperCaseShortenedName;
-    private final String lowerCaseFullName;
-    private final String upperCaseFirstFullName;
-    private final String upperCaseFullName;
-    private final String chinese;
+    /**
+     * 毫秒单位
+     */
+    public static final int MILLISECOND_UNIT = 1000;
+
+    /**
+     * 秒单位
+     */
+    public static final int SECOND_UNIT = 60;
+
+    /**
+     * 分钟单位
+     */
+    public static final short MINUTE_UNIT = 60;
+
+    /**
+     * 小时单位
+     */
+    public static final short HOUR_UNIT = 24;
+
+    private final String name1;
+    private final String name2;
+    private final String name3;
+    private final String name4;
+    private final String name5;
+    private final String name6;
+    private final String name7;
 
 }
