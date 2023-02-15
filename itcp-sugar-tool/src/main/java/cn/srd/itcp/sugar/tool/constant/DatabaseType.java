70964acde7d3893510013707e8018b0050e39838
wjmwss <wjmwss@outlook.com>
2023-02-15 18:58:56 +0800

    - feature: 增加数据库常量；

---
+++ itcp-sugar-tool/src/main/java/cn/srd/itcp/sugar/tool/constant/DatabaseType.java
@@ -0,0 +1,16 @@
+package cn.srd.itcp.sugar.tool.constant;
+
+/**
+ * 数据库常量
+ *
+ * @author wjm
+ * @since 2023-02-11 11:36:22
+ */
+public class DatabaseType {
+
+    /**
+     * postgresql
+     */
+    public static final String POSTGRE_SQL = "postgresql";
+
+}
