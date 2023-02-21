2ad4ccaea8cad30b3821720d762f30b109516fa0
wjmwss <wjmwss@outlook.com>
2023-02-21 10:37:07 +0800

    - feature: 增加 protobuf 生成代码使用示例；

---
+++ itcp-sugar-protocol/itcp-sugar-protocol-protobuf/src/main/java/cn/srd/itcp/sugar/protocol/protobuf/SugarProtobufApplication.java
@@ -0,0 +1,16 @@
+package cn.srd.itcp.sugar.protocol.protobuf;
+
+/**
+ * boot
+ */
+public class SugarProtobufApplication {
+
+    /**
+     * main
+     *
+     * @param args main argument
+     */
+    public static void main(String[] args) {
+    }
+
+}
