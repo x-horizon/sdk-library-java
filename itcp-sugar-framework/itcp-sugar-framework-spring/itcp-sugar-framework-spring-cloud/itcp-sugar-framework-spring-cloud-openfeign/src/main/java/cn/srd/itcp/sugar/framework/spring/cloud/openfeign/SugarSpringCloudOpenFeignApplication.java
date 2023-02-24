73aec342aa58940eb81c1735bae9ec275e88ef98
wjmwss <wjmwss@outlook.com>
2023-02-24 15:07:27 +0800

    - feature: 增加 openfeign 模块；

---
+++ itcp-sugar-framework/itcp-sugar-framework-spring/itcp-sugar-framework-spring-cloud/itcp-sugar-framework-spring-cloud-openfeign/src/main/java/cn/srd/itcp/sugar/framework/spring/cloud/openfeign/SugarSpringCloudOpenFeignApplication.java
@@ -0,0 +1,21 @@
+package cn.srd.itcp.sugar.framework.spring.cloud.openfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * boot
+ */
+@SpringBootApplication
+public class SugarSpringCloudOpenFeignApplication {
+
+    /**
+     * main
+     *
+     * @param args main argument
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SugarSpringCloudOpenFeignApplication.class, args);
+    }
+
+}
