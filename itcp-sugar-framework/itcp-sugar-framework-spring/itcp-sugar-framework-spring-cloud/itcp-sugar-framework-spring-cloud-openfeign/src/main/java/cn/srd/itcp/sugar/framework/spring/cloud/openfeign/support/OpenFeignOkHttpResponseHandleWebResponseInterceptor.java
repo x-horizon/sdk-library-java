8529fef3bb0d790fc7adc8e5cea6d79d022da952
wjmwss <wjmwss@outlook.com>
2023-03-04 18:33:27 +0800

    - feature: 支持 openfeign 全局拦截 webResponse 的拦截器；

---
+++ itcp-sugar-framework/itcp-sugar-framework-spring/itcp-sugar-framework-spring-cloud/itcp-sugar-framework-spring-cloud-openfeign/src/main/java/cn/srd/itcp/sugar/framework/spring/cloud/openfeign/support/OpenFeignOkHttpResponseHandleWebResponseInterceptor.java
@@ -0,0 +1,34 @@
+package cn.srd.itcp.sugar.framework.spring.cloud.openfeign.support;
+
+import cn.srd.itcp.sugar.component.convert.all.core.Converts;
+import cn.srd.itcp.sugar.tool.exceptions.RunningException;
+import cn.srd.itcp.sugar.tool.web.WebResponse;
+import lombok.SneakyThrows;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+/**
+ * open feign okhttp 响应拦截器，用于拦截 {@link WebResponse}
+ *
+ * @author wjm
+ * @since 2023-03-04 16:48:19
+ */
+public class OpenFeignOkHttpResponseHandleWebResponseInterceptor implements OpenFeignOkHttpResponseInterceptor<WebResponse<?>> {
+
+    @SneakyThrows
+    @Override
+    public WebResponse<?> parseProductionResponse(Response response) {
+        return Converts.withJackson().toBean(response.body().string(), WebResponse.class);
+    }
+
+    @Override
+    public Response ofResponse(Response response, WebResponse<?> productionResponse) {
+        if (productionResponse.errorIs()) {
+            throw new RunningException(productionResponse.getStatus(), productionResponse.getMessage());
+        }
+        return response.newBuilder()
+                .body(ResponseBody.create(Converts.withJackson().toString(productionResponse.getData()), response.body().contentType()))
+                .build();
+    }
+
+}
---
+++ itcp-sugar-framework/itcp-sugar-framework-spring/itcp-sugar-framework-spring-cloud/itcp-sugar-framework-spring-cloud-openfeign/src/main/java/cn/srd/itcp/sugar/framework/spring/cloud/openfeign/support/OpenFeignOkHttpResponseInterceptor.java
@@ -0,0 +1,41 @@
+package cn.srd.itcp.sugar.framework.spring.cloud.openfeign.support;
+
+import lombok.SneakyThrows;
+import okhttp3.Interceptor;
+import okhttp3.Response;
+import org.springframework.lang.NonNull;
+
+/**
+ * open feign okhttp 响应拦截器
+ *
+ * @author wjm
+ * @since 2023-03-04 16:48:19
+ */
+public interface OpenFeignOkHttpResponseInterceptor<T> extends Interceptor {
+
+    /**
+     * 解析生产端响应结果
+     *
+     * @param response okhttp response
+     * @return 生产端响应结果
+     */
+    T parseProductionResponse(Response response);
+
+    /**
+     * 构造 okhttp response
+     *
+     * @param response           okhttp response
+     * @param productionResponse 生产端响应结果
+     * @return okhttp response
+     */
+    Response ofResponse(Response response, T productionResponse);
+
+    @SneakyThrows
+    @NonNull
+    @Override
+    default Response intercept(Chain chain) {
+        Response response = chain.proceed(chain.request());
+        return ofResponse(response, parseProductionResponse(response));
+    }
+
+}
