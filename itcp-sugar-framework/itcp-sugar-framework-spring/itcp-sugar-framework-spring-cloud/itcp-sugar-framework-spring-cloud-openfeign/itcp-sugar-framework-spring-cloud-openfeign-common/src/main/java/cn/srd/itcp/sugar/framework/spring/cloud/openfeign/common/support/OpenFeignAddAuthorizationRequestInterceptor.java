fb73dabd7b67030c7ff6f0fe7a3ee36fe4840e12
wjmwss <wjmwss@outlook.com>
2023-03-06 15:39:54 +0800

    - feature: 新增 open feign common 模块；

---
+++ itcp-sugar-framework/itcp-sugar-framework-spring/itcp-sugar-framework-spring-cloud/itcp-sugar-framework-spring-cloud-openfeign/itcp-sugar-framework-spring-cloud-openfeign-common/src/main/java/cn/srd/itcp/sugar/framework/spring/cloud/openfeign/common/support/OpenFeignAddAuthorizationRequestInterceptor.java
@@ -0,0 +1,25 @@
+package cn.srd.itcp.sugar.framework.spring.cloud.openfeign.common.support;
+
+import cn.srd.itcp.sugar.framework.spring.tool.webmvc.core.SpringsWebMVCUtil;
+import cn.srd.itcp.sugar.tool.core.Objects;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.http.HttpHeaders;
+
+/**
+ * open feign okhttp 请求拦截器：为请求头添加 {@link HttpHeaders#AUTHORIZATION}
+ *
+ * @author wjm
+ * @since 2023-03-06 15:07:11
+ */
+public class OpenFeignAddAuthorizationRequestInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        String token = SpringsWebMVCUtil.getHttpServletRequest().getHeader(HttpHeaders.AUTHORIZATION);
+        if (Objects.isNotBlank(token)) {
+            requestTemplate.header(HttpHeaders.AUTHORIZATION, token);
+        }
+    }
+
+}
