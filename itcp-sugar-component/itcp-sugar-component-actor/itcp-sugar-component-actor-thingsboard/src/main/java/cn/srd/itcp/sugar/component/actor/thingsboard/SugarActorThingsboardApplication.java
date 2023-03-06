ee419d7db87bb5987b5dabc6a6be40760986acc5
wjmwss <wjmwss@outlook.com>
2023-03-06 14:47:25 +0800

    - feature: 新增 thingsboard actor 模块；

---
+++ itcp-sugar-component/itcp-sugar-component-actor/itcp-sugar-component-actor-thingsboard/src/main/java/cn/srd/itcp/sugar/component/actor/thingsboard/SugarActorThingsboardApplication.java
@@ -0,0 +1,16 @@
+package cn.srd.itcp.sugar.component.actor.thingsboard;
+
+/**
+ * boot
+ */
+public class SugarActorThingsboardApplication {
+
+    /**
+     * main
+     *
+     * @param args main argument
+     */
+    public static void main(String[] args) {
+    }
+
+}
