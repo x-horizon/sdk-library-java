6797ed6f542b2d4e721d3232da2a1ab89451fa4c
wjmwss <wjmwss@outlook.com>
2023-03-14 21:20:20 +0800

    - feature: 增加 SQL 常量池；

---
+++ sugar-tool/src/main/java/cn/srd/itcp/sugar/tool/constant/SQLPool.java
@@ -0,0 +1,21 @@
+package cn.srd.itcp.sugar.tool.constant;
+
+/**
+ * SQL 常量池
+ *
+ * @author wjm
+ * @since 2023-03-14 15:39:11
+ */
+public class SQLPool {
+
+    /**
+     * SELECT
+     */
+    public static final String SELECT = "SELECT";
+
+    /**
+     * FROM
+     */
+    public static final String FROM = "FROM";
+
+}
