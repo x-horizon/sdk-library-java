36a633331466fd2c27cc7c26b3d1c5093c7b63c9
wjmwss <wjmwss@outlook.com>
2023-03-23 21:51:21 +0800

    - feature: 添加通用枚举库；

---
+++ sugar-tool/src/test/java/cn/srd/itcp/sugar/tool/core/Main.java
@@ -0,0 +1,27 @@
+// package cn.srd.itcp.sugar.tool.core;
+//
+// /**
+//  * generic enum example, copy from <a href="https://github.com/cmoine/generic-enums">generic-enums</a>
+//  *
+//  * @author wjm
+//  * @since 2023-03-23 21:34:12
+//  */
+// class MyParameterSer {
+//     int getInt(Parameters<Integer> param) {
+//         // Use you deserialization mechanism...
+//     }
+//
+//     void setInt(Parameters<Integer> param, int value) {
+//         // Use you Serialization mechanism...
+//     }
+//
+//     // And so on for all supported types
+// }
+//
+// public class Main {
+//     public static void main(String[] args) {
+//         MyParameterSer parameterSer = new MyParameterSer();
+//         parameterSer.setInt(ParametersExt.INT, 99);
+//         parameterSer.setInt(ParametersExt.BOOLEAN, 99); // ends up in a compilation error !
+//     }
+// }
