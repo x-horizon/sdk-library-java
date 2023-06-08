333478636d7320e40570d867dbbccce109f53b43
wjmwss <wjmwss@>
2023-06-08 23:56:58 +0800

    - style: 注释无用代码；

--- sugar-cache/cache-all/src/main/java/cn/srd/itcp/sugar/cache/all/support/ConditionOnEnableMultilevelCaching.java
+++ sugar-cache/cache-all/src/main/java/cn/srd/itcp/sugar/cache/all/support/ConditionOnEnableMultilevelCaching.java
@@ -1,16 +1,16 @@
-package cn.srd.itcp.sugar.cache.all.support;
-
-import org.springframework.context.annotation.Conditional;
-
-import java.lang.annotation.*;
-
-/**
- * @author wjm
- * @since 2023-06-07 16:48:52
- */
-@Target({ElementType.TYPE, ElementType.METHOD})
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-@Conditional(EnableMultilevelCachingCondition.class)
-public @interface ConditionOnEnableMultilevelCaching {
-}
+//package cn.srd.itcp.sugar.cache.all.support;
+//
+//import org.springframework.context.annotation.Conditional;
+//
+//import java.lang.annotation.*;
+//
+///**
+// * @author wjm
+// * @since 2023-06-07 16:48:52
+// */
+//@Target({ElementType.TYPE, ElementType.METHOD})
+//@Retention(RetentionPolicy.RUNTIME)
+//@Documented
+//@Conditional(EnableMultilevelCachingCondition.class)
+//public @interface ConditionOnEnableMultilevelCaching {
+//}
