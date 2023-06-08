6cbe8a3ca891e64c69a22850ab77392e83a8c60b
wjmwss <wjm@srd.cn>
2023-06-08 22:15:35 +0800

    - optimize: optimize filter；

--- sugar-tool/src/main/java/cn/srd/itcp/sugar/tool/core/CollectionsUtil.java
+++ sugar-tool/src/main/java/cn/srd/itcp/sugar/tool/core/CollectionsUtil.java
@@ -690,6 +690,23 @@ public class CollectionsUtil extends CollUtil {
     }
 
     /**
+     * 过滤掉 集合 中 value 为 null 或指定类型的元素
+     *
+     * @param from           输入参数
+     * @param specifiedClass 指定类型
+     * @param <T>            元素类型
+     * @return 过滤后集合
+     */
+    public static <T> List<T> filterNullAndSpecifiedClass(@Nullable Collection<T> from, @NonNull Class<?> specifiedClass) {
+        if (Objects.isEmpty(from)) {
+            return new ArrayList<>();
+        }
+        return from.stream()
+                .filter(item -> Objects.isNotNull(item) && Objects.notEquals(specifiedClass, item.getClass()))
+                .toList();
+    }
+
+    /**
      * 过滤掉 Map 中 value 为 null 或指定类型的元素
      *
      * @param from           输入参数
@@ -702,13 +719,9 @@ public class CollectionsUtil extends CollUtil {
         if (Objects.isEmpty(from)) {
             return new HashMap<>();
         }
-        Map<K, V> output = new HashMap<>();
-        from.forEach((key, value) -> {
-            if (Objects.isNotNull(value) && Objects.notEquals(specifiedClass, value.getClass())) {
-                output.put(key, value);
-            }
-        });
-        return output;
+        return from.entrySet().stream()
+                .filter(entry -> Objects.isNotNull(entry.getValue()) && Objects.notEquals(specifiedClass, entry.getValue().getClass()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     // ==================================== 集合去重 ====================================
