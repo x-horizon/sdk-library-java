3e686e6665a5b60933a59dafd9aa8e383d1d0a66
wjmwss <wjm@srd.cn>
2023-06-13 16:38:44 +0800

    - style: remove useless code;

--- sugar-cache/cache-all/src/main/java/cn/srd/itcp/sugar/cache/all/support/CacheAllAutoConfiguration.java
+++ sugar-cache/cache-all/src/main/java/cn/srd/itcp/sugar/cache/all/support/CacheAllAutoConfiguration.java
@@ -10,9 +10,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
  * TODO wjm 未完成：
  * TODO wjm 1、多节点更新缓存时的 serverId
  * TODO wjm 2、RedissonTopics.addListener
- * TODO wjm 3、考虑 key value 为空的问题
- * TODO wjm 4、考虑 线程安全问题
- * TODO wjm 5、时间有问题：DurationWrapper result25 = cache.getExpirationTime(CACHE_NAME1); DurationWrapper result26 = cache.getTimeToLive(CACHE_NAME1);
  * <p>
  * {@link EnableAutoConfiguration AutoConfiguration} for Sugar Cache All
  *
