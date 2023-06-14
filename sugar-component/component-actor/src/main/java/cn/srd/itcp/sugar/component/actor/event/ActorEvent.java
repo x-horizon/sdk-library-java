de8424b65ecefb5b3f08874273415cb9b2fa4f47
wjmwss <wjmwss@>
2023-06-15 01:32:02 +0800

    - fix: fix process by other actors and self logic；

--- sugar-component/component-actor/src/main/java/cn/srd/itcp/sugar/component/actor/event/ActorEvent.java
+++ sugar-component/component-actor/src/main/java/cn/srd/itcp/sugar/component/actor/event/ActorEvent.java
@@ -36,21 +36,21 @@ public interface ActorEvent<T> extends Serializable {
     List<ActorType> getBindActorTypes();
 
     /**
-     * 事件分发
-     *
-     * @return TODO wjm 待定义
-     */
-    default boolean processByOtherActors() {
-        getBindActorTypes().forEach(actorType -> actorType.getStrategy().getMailbox().tell(this));
-        return true;
-    }
-
-    /**
      * 事件自治
      *
      * @return TODO wjm 待定义
      */
     default boolean processBySelf() {
+        getEventType().getStrategy().process(this);
+        return true;
+    }
+
+    /**
+     * 事件分发
+     *
+     * @return TODO wjm 待定义
+     */
+    default boolean processByOtherActors() {
         getBindActorTypes().forEach(actorType -> actorType.getStrategy().getMailbox().tell(this));
         return true;
     }
