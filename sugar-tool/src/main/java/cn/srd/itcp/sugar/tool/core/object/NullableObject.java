0ec6da657c898516ffc0cffeedeeed1f7aed9ecc
wjmwss <wjm@srd.cn>
2023-06-16 15:44:55 +0800

    - optimize: keep min responsibility;

--- sugar-tool/src/main/java/cn/srd/itcp/sugar/tool/core/object/NullableObject.java
+++ sugar-tool/src/main/java/cn/srd/itcp/sugar/tool/core/object/NullableObject.java
@@ -2,15 +2,13 @@ package cn.srd.itcp.sugar.tool.core.object;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import java.io.Serializable;
-
 /**
  * an object to define what is null
  *
  * @author wjm
  * @since 2023-06-14 8:49:19
  */
-public interface NullableObject extends Serializable {
+public interface NullableObject {
 
     /**
      * is it null
