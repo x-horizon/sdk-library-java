61d978a98678bf6b7c7152ca9cc300d3ec17cb10
王钧明 <wjmwss@outlook.com>
2023-06-21 14:31:29 +0800

    - feature: support string and local time convert;

---
+++ sugar-component/component-convert/convert-jackson/src/main/java/cn/srd/itcp/sugar/component/convert/jackson/support/JacksonLocalTimeToStringHourMinuteSecondSerializer.java
@@ -0,0 +1,25 @@
+package cn.srd.itcp.sugar.component.convert.jackson.support;
+
+import cn.srd.itcp.sugar.tool.core.time.TimeUtil;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import lombok.SneakyThrows;
+
+import java.time.LocalTime;
+
+/**
+ * Jackson 序列化处理器：LocalTime =&gt; String，如：14:12:11
+ *
+ * @author wjm
+ * @since 2023-06-21 12:01:01
+ */
+public class JacksonLocalTimeToStringHourMinuteSecondSerializer extends JsonSerializer<LocalTime> {
+
+    @SneakyThrows
+    @Override
+    public void serialize(LocalTime from, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) {
+        jsonGenerator.writeObject(TimeUtil.toStringHourMinuteSecond(from));
+    }
+
+}
