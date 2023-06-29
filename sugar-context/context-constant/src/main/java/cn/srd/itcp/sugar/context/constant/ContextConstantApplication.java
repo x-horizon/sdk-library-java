baa4e0fb6b8aad018edd7f2d025f4b213aa39545
jimmy <wjmwss@outlook.com>
2023-06-29 20:35:45 +0800

    - feature: add the context constant module;

---
+++ sugar-context/context-constant/src/main/java/cn/srd/itcp/sugar/context/constant/ContextConstantApplication.java
@@ -0,0 +1,16 @@
+package cn.srd.itcp.sugar.context.constant;
+
+/**
+ * boot
+ */
+public class ContextConstantApplication {
+
+    /**
+     * main
+     *
+     * @param args main argument
+     */
+    public static void main(String[] args) {
+    }
+
+}
---
+++ sugar-context/context-constant/src/main/java/cn/srd/itcp/sugar/context/constant/core/ModuleConstant.java
@@ -0,0 +1,17 @@
+package cn.srd.itcp.sugar.context.constant.core;
+
+/**
+ * all module name of sugar
+ *
+ * @author wjm
+ * @since 2023-06-29 20:25:44
+ */
+public final class ModuleConstant {
+
+    public static final String ACTOR_SYSTEM = "Actor System - ";
+
+    public static final String CACHE_SYSTEM = "Cache System - ";
+
+    public static final String ENUM_AUTOWIRED_SYSTEM = "Enum Autowired System - ";
+
+}
