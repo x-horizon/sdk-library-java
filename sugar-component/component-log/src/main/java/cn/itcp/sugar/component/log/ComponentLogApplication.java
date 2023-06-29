aa024bfa13bfb314cd9348ccd27f6244773000a8
jimmy <wjmwss@outlook.com>
2023-06-29 20:24:32 +0800

    - fix: fix the wrong ignore folder;

---
+++ sugar-component/component-log/src/main/java/cn/itcp/sugar/component/log/ComponentLogApplication.java
@@ -0,0 +1,16 @@
+package cn.itcp.sugar.component.log;
+
+/**
+ * boot
+ */
+public class ComponentLogApplication {
+
+    /**
+     * main
+     *
+     * @param args main argument
+     */
+    public static void main(String[] args) {
+    }
+
+}
