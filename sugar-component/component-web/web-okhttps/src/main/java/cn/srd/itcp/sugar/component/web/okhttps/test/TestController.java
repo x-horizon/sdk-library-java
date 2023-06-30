194f164a32c435921d11e21d4288b6534b422e7d
jimmy <wjmwss@outlook.com>
2023-06-30 23:06:24 +0800

    - feature: support okhttps;

---
+++ sugar-component/component-web/web-okhttps/src/main/java/cn/srd/itcp/sugar/component/web/okhttps/test/TestController.java
@@ -0,0 +1,16 @@
+// package cn.srd.itcp.sugar.component.web.okhttps.test;
+//
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+//
+// @RestController
+// @RequestMapping("/sugar/okhttp")
+// public class TestController {
+//
+//     @PostMapping("/test")
+//     public String test() {
+//         return "success";
+//     }
+//
+// }
