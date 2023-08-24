0c9c64515c95f4d5e26b80b60dff32a877da36b9
jimmy <wjmwss@outlook.com>
2023-08-25 00:12:32 +0800

    - style: add test data；

---
+++ orm/orm-mybatis-flex/src/test/cmd/sql/test-mybatis-flex.sql
@@ -0,0 +1,44 @@
+CREATE TABLE student
+(
+    student_id          BIGINT                                             NOT NULL,
+    student_detail_info JSONB                          DEFAULT '{}'::JSONB NOT NULL,
+    student_class_infos JSONB                          DEFAULT '[]'::JSONB NOT NULL,
+    student_type        SMALLINT                       DEFAULT 0           NOT NULL,
+    creator_id          BIGINT                                             NOT NULL,
+    create_time         TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT NOW()       NOT NULL,
+    row_is_delete       BOOLEAN                        DEFAULT FALSE       NOT NULL,
+    PRIMARY KEY (student_id)
+);
+
+CREATE TABLE class
+(
+    class_id      BIGINT                                       NOT NULL,
+    class_name    VARCHAR(50)                    DEFAULT ''    NOT NULL,
+    creator_id    BIGINT                                       NOT NULL,
+    create_time   TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
+    row_is_delete BOOLEAN                        DEFAULT FALSE NOT NULL,
+    PRIMARY KEY (class_id)
+);
+
+CREATE TABLE student_class
+(
+    relation_id   BIGINT                                       NOT NULL,
+    student_id    BIGINT                                       NOT NULL,
+    class_id      BIGINT                                       NOT NULL,
+    creator_id    BIGINT                                       NOT NULL,
+    create_time   TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
+    row_is_delete BOOLEAN                        DEFAULT FALSE NOT NULL,
+    PRIMARY KEY (relation_id)
+);
+
+CREATE TABLE book
+(
+    book_id       BIGINT                                       NOT NULL,
+    student_id    BIGINT                                       NOT NULL,
+    book_name     VARCHAR(50)                    DEFAULT ''    NOT NULL,
+    creator_id    BIGINT                                       NOT NULL,
+    create_time   TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
+    row_is_delete BOOLEAN                        DEFAULT FALSE NOT NULL,
+    PRIMARY KEY (book_id)
+);
+
