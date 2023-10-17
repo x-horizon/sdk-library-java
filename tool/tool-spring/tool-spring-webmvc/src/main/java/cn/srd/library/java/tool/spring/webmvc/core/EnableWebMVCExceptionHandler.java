3315a57456b9b0393e699dab7fe391fc94b043d7
jimmy <wjmwss@outlook.com>
2023-10-17 20:49:44 +0800

    - style: 恢复原本类名；

--- tool/tool-spring/tool-spring-webmvc/src/main/java/cn/srd/library/java/tool/spring/webmvc/core/EnableWebMVCExceptionHandler.java
+++ tool/tool-spring/tool-spring-webmvc/src/main/java/cn/srd/library/java/tool/spring/webmvc/core/EnableWebMVCExceptionHandler.java
@@ -1,12 +1,12 @@
 package cn.srd.library.java.tool.spring.webmvc.core;
 
-import cn.srd.library.java.tool.spring.webmvc.support.SpringWebMVCExceptionHandler;
+import cn.srd.library.java.tool.spring.webmvc.support.WebMVCExceptionHandler;
 import org.springframework.context.annotation.Import;
 
 import java.lang.annotation.*;
 
 /**
- * 启用 {@link SpringWebMVCExceptionHandler} 的功能
+ * 启用 {@link WebMVCExceptionHandler} 的功能
  *
  * @author wjm
  * @since 2022-07-16 18:16:22
@@ -14,7 +14,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Documented
-@Import(SpringWebMVCExceptionHandler.class)
-public @interface EnableSpringWebMVCExceptionHandler {
+@Import(WebMVCExceptionHandler.class)
+public @interface EnableWebMVCExceptionHandler {
 
 }
