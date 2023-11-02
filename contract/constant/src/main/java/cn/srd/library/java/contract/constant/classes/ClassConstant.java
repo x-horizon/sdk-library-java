2505b776b0a87a93651c65bc01505a1e78600fbc
jimmy <wjmwss@outlook.com>
2023-11-02 16:24:02 +0800

    - optimize: 支持更多的 ant path 筛选；

--- contract/constant/src/main/java/cn/srd/library/java/contract/constant/classes/ClassConstant.java
+++ contract/constant/src/main/java/cn/srd/library/java/contract/constant/classes/ClassConstant.java
@@ -7,6 +7,8 @@ package cn.srd.library.java.contract.constant.classes;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.util.Set;
+
 /**
  * class constant
  *
@@ -20,8 +22,16 @@ public class ClassConstant {
 
     public static final String REFERENCE_JAR_PATH = ".jar!/";
 
+    public static final String JAVA_TEST_PATH = "/java/test/";
+
     public static final String JAVA_MAIN_PATH = "/java/main/";
 
-    public static final String JAVA_TEST_PATH = "/java/test/";
+    public static final String RESOURCE_MAIN_PATH = "/resources/main/";
+
+    public static final String DOCKER_APP_CLASS_PATH = "/app/classes/";
+
+    public static final String DOCKER_APP_RESOURCE_PATH = "/app/resources/";
+
+    public static final Set<String> SOURCE_ROOT_PATHS = Set.of(JAVA_MAIN_PATH, RESOURCE_MAIN_PATH, DOCKER_APP_CLASS_PATH, DOCKER_APP_RESOURCE_PATH);
 
 }
