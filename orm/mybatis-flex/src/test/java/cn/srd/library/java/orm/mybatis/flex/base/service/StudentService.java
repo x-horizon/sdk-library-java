ec213dfc7b201133fdc011bee4c3da0c08c651a1
jimmy <wjmwss@outlook.com>
2023-11-10 22:20:37 +0800

    - style: 优化包路径；

--- orm/mybatis-flex/src/test/java/cn/srd/library/java/orm/mybatis/flex/base/service/StudentService.java
+++
@@ -1,8 +0,0 @@
-package cn.srd.library.java.orm.mybatis.flex.base.service;
-
-import cn.srd.library.java.orm.mybatis.flex.base.model.po.StudentPO;
-import com.mybatisflex.core.service.IService;
-
-public interface StudentService extends IService<StudentPO> {
-
-}
