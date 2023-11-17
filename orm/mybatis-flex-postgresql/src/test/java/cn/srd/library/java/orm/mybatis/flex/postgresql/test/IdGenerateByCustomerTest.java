23390c9d497fa5f6439b95b13d9f912ec84e6773
jimmy <wjmwss@outlook.com>
2023-11-17 09:58:40 +0800

    - style: unit test；

--- orm/mybatis-flex-postgresql/src/test/java/cn/srd/library/java/orm/mybatis/flex/postgresql/test/IdGenerateByCustomerTest.java
+++ orm/mybatis-flex-postgresql/src/test/java/cn/srd/library/java/orm/mybatis/flex/postgresql/test/IdGenerateByCustomerTest.java
@@ -1,54 +1,54 @@
-package cn.srd.library.java.orm.mybatis.flex.postgresql.test;
-
-import cn.srd.library.java.contract.constant.booleans.BooleanConstant;
-import cn.srd.library.java.orm.mybatis.flex.base.audit.AuditLogConfig;
-import cn.srd.library.java.orm.mybatis.flex.base.autoconfigure.EnableMybatisFlexCustomizer;
-import cn.srd.library.java.orm.mybatis.flex.base.id.IdConfig;
-import cn.srd.library.java.orm.mybatis.flex.base.id.IdGenerateType;
-import cn.srd.library.java.orm.mybatis.flex.base.id.IdGenerator;
-import cn.srd.library.java.orm.mybatis.flex.base.listener.ListenerConfig;
-import cn.srd.library.java.orm.mybatis.flex.base.logic.DeleteLogicConfig;
-import cn.srd.library.java.orm.mybatis.flex.postgresql.config.TestInsertListener;
-import cn.srd.library.java.orm.mybatis.flex.postgresql.config.TestUpdateListener;
-import cn.srd.library.java.orm.mybatis.flex.postgresql.dao.StudentTestIdCustomerDao;
-import cn.srd.library.java.orm.mybatis.flex.postgresql.model.po.StudentTestIdCustomerPO;
-import cn.srd.library.java.tool.lang.random.Randoms;
-import lombok.AllArgsConstructor;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@AllArgsConstructor
-@MapperScan("cn.srd.library.java.orm.mybatis.flex.postgresql.dao")
-@EnableMybatisFlexCustomizer(
-        globalIdGenerateConfig = @IdConfig(generateType = IdGenerateType.CUSTOMER, generator = IdGenerateByCustomerTest.MyIdGenerator.class),
-        globalDeleteLogicConfig = @DeleteLogicConfig(normalValue = BooleanConstant.FALSE, deletedValue = BooleanConstant.TRUE),
-        globalListenerConfig = @ListenerConfig(whenInsert = TestInsertListener.class, whenUpdate = TestUpdateListener.class),
-        globalAuditConfig = @AuditLogConfig(enable = true)
-)
-@SpringBootTest
-@RunWith(SpringRunner.class)
-public class IdGenerateByCustomerTest {
-
-    private final StudentTestIdCustomerDao studentTestIdCustomerDao;
-
-    @Test
-    public void testIt() {
-        // studentTestIdCustomerDao.insert(StudentTestIdCustomerPO.builder().build());
-        studentTestIdCustomerDao.insertSelective(StudentTestIdCustomerPO.builder().build());
-
-        System.out.println();
-    }
-
-    public static class MyIdGenerator implements IdGenerator {
-
-        @Override
-        public Object generate(Object entity, String keyColumn) {
-            return Randoms.randomString(20);
-        }
-
-    }
-
-}
+// package cn.srd.library.java.orm.mybatis.flex.postgresql.test;
+//
+// import cn.srd.library.java.contract.constant.booleans.BooleanConstant;
+// import cn.srd.library.java.orm.mybatis.flex.base.audit.AuditLogConfig;
+// import cn.srd.library.java.orm.mybatis.flex.base.autoconfigure.EnableMybatisFlexCustomizer;
+// import cn.srd.library.java.orm.mybatis.flex.base.id.IdConfig;
+// import cn.srd.library.java.orm.mybatis.flex.base.id.IdGenerateType;
+// import cn.srd.library.java.orm.mybatis.flex.base.id.IdGenerator;
+// import cn.srd.library.java.orm.mybatis.flex.base.listener.ListenerConfig;
+// import cn.srd.library.java.orm.mybatis.flex.base.logic.DeleteLogicConfig;
+// import cn.srd.library.java.orm.mybatis.flex.postgresql.config.TestInsertListener;
+// import cn.srd.library.java.orm.mybatis.flex.postgresql.config.TestUpdateListener;
+// import cn.srd.library.java.orm.mybatis.flex.postgresql.dao.StudentTestIdCustomerDao;
+// import cn.srd.library.java.orm.mybatis.flex.postgresql.model.po.StudentTestIdCustomerPO;
+// import cn.srd.library.java.tool.lang.random.Randoms;
+// import lombok.AllArgsConstructor;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.mybatis.spring.annotation.MapperScan;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
+//
+// @AllArgsConstructor
+// @MapperScan("cn.srd.library.java.orm.mybatis.flex.postgresql.dao")
+// @EnableMybatisFlexCustomizer(
+//         globalIdGenerateConfig = @IdConfig(generateType = IdGenerateType.CUSTOMER, generator = IdGenerateByCustomerTest.MyIdGenerator.class),
+//         globalDeleteLogicConfig = @DeleteLogicConfig(normalValue = BooleanConstant.FALSE, deletedValue = BooleanConstant.TRUE),
+//         globalListenerConfig = @ListenerConfig(whenInsert = TestInsertListener.class, whenUpdate = TestUpdateListener.class),
+//         globalAuditConfig = @AuditLogConfig(enable = true)
+// )
+// @SpringBootTest
+// @RunWith(SpringRunner.class)
+// public class IdGenerateByCustomerTest {
+//
+//     private final StudentTestIdCustomerDao studentTestIdCustomerDao;
+//
+//     @Test
+//     public void testIt() {
+//         // studentTestIdCustomerDao.insert(StudentTestIdCustomerPO.builder().build());
+//         studentTestIdCustomerDao.insertSelective(StudentTestIdCustomerPO.builder().build());
+//
+//         System.out.println();
+//     }
+//
+//     public static class MyIdGenerator implements IdGenerator {
+//
+//         @Override
+//         public Object generate(Object entity, String keyColumn) {
+//             return Randoms.randomString(20);
+//         }
+//
+//     }
+//
+// }
--- orm/mybatis-flex-postgresql/src/test/java/cn/srd/library/java/orm/mybatis/flex/postgresql/test/IdGenerateByUncontrolledTest.java
+++ orm/mybatis-flex-postgresql/src/test/java/cn/srd/library/java/orm/mybatis/flex/postgresql/test/IdGenerateByUncontrolledTest.java
@@ -1,44 +1,44 @@
-package cn.srd.library.java.orm.mybatis.flex.postgresql.test;
-
-import cn.srd.library.java.contract.constant.booleans.BooleanConstant;
-import cn.srd.library.java.orm.mybatis.flex.base.audit.AuditLogConfig;
-import cn.srd.library.java.orm.mybatis.flex.base.autoconfigure.EnableMybatisFlexCustomizer;
-import cn.srd.library.java.orm.mybatis.flex.base.id.IdConfig;
-import cn.srd.library.java.orm.mybatis.flex.base.id.IdGenerateType;
-import cn.srd.library.java.orm.mybatis.flex.base.listener.ListenerConfig;
-import cn.srd.library.java.orm.mybatis.flex.base.logic.DeleteLogicConfig;
-import cn.srd.library.java.orm.mybatis.flex.postgresql.config.TestInsertListener;
-import cn.srd.library.java.orm.mybatis.flex.postgresql.config.TestUpdateListener;
-import cn.srd.library.java.orm.mybatis.flex.postgresql.dao.StudentTestIdUncontrolledDao;
-import cn.srd.library.java.orm.mybatis.flex.postgresql.model.po.StudentTestIdUncontrolledPO;
-import cn.srd.library.java.tool.lang.random.Randoms;
-import lombok.AllArgsConstructor;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@AllArgsConstructor
-@MapperScan("cn.srd.library.java.orm.mybatis.flex.postgresql.dao")
-@EnableMybatisFlexCustomizer(
-        globalIdGenerateConfig = @IdConfig(generateType = IdGenerateType.UNCONTROLLED),
-        globalDeleteLogicConfig = @DeleteLogicConfig(normalValue = BooleanConstant.FALSE, deletedValue = BooleanConstant.TRUE),
-        globalListenerConfig = @ListenerConfig(whenInsert = TestInsertListener.class, whenUpdate = TestUpdateListener.class),
-        globalAuditConfig = @AuditLogConfig(enable = true)
-)
-@SpringBootTest
-@RunWith(SpringRunner.class)
-public class IdGenerateByUncontrolledTest {
-
-    private final StudentTestIdUncontrolledDao studentTestIdUncontrolledDao;
-
-    @Test
-    public void testIt() {
-        // studentTestIdUncontrolledDao.insert(StudentTestIdUncontrolledPO.builder().id(Randoms.randomString(10)).build());
-        studentTestIdUncontrolledDao.insertSelective(StudentTestIdUncontrolledPO.builder().id(Randoms.randomString(10)).build());
-
-        System.out.println();
-    }
-
-}
+// package cn.srd.library.java.orm.mybatis.flex.postgresql.test;
+//
+// import cn.srd.library.java.contract.constant.booleans.BooleanConstant;
+// import cn.srd.library.java.orm.mybatis.flex.base.audit.AuditLogConfig;
+// import cn.srd.library.java.orm.mybatis.flex.base.autoconfigure.EnableMybatisFlexCustomizer;
+// import cn.srd.library.java.orm.mybatis.flex.base.id.IdConfig;
+// import cn.srd.library.java.orm.mybatis.flex.base.id.IdGenerateType;
+// import cn.srd.library.java.orm.mybatis.flex.base.listener.ListenerConfig;
+// import cn.srd.library.java.orm.mybatis.flex.base.logic.DeleteLogicConfig;
+// import cn.srd.library.java.orm.mybatis.flex.postgresql.config.TestInsertListener;
+// import cn.srd.library.java.orm.mybatis.flex.postgresql.config.TestUpdateListener;
+// import cn.srd.library.java.orm.mybatis.flex.postgresql.dao.StudentTestIdUncontrolledDao;
+// import cn.srd.library.java.orm.mybatis.flex.postgresql.model.po.StudentTestIdUncontrolledPO;
+// import cn.srd.library.java.tool.lang.random.Randoms;
+// import lombok.AllArgsConstructor;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.mybatis.spring.annotation.MapperScan;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
+//
+// @AllArgsConstructor
+// @MapperScan("cn.srd.library.java.orm.mybatis.flex.postgresql.dao")
+// @EnableMybatisFlexCustomizer(
+//         globalIdGenerateConfig = @IdConfig(generateType = IdGenerateType.UNCONTROLLED),
+//         globalDeleteLogicConfig = @DeleteLogicConfig(normalValue = BooleanConstant.FALSE, deletedValue = BooleanConstant.TRUE),
+//         globalListenerConfig = @ListenerConfig(whenInsert = TestInsertListener.class, whenUpdate = TestUpdateListener.class),
+//         globalAuditConfig = @AuditLogConfig(enable = true)
+// )
+// @SpringBootTest
+// @RunWith(SpringRunner.class)
+// public class IdGenerateByUncontrolledTest {
+//
+//     private final StudentTestIdUncontrolledDao studentTestIdUncontrolledDao;
+//
+//     @Test
+//     public void testIt() {
+//         // studentTestIdUncontrolledDao.insert(StudentTestIdUncontrolledPO.builder().id(Randoms.randomString(10)).build());
+//         studentTestIdUncontrolledDao.insertSelective(StudentTestIdUncontrolledPO.builder().id(Randoms.randomString(10)).build());
+//
+//         System.out.println();
+//     }
+//
+// }
