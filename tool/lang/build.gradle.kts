a969c4664db7d15193f9a8b8edf317943dc1421b
jimmy <wjmwss@outlook.com>
2023-11-18 09:25:40 +0800

    - style: 移除无用代码；

--- tool/lang/build.gradle.kts
+++ tool/lang/build.gradle.kts
@@ -7,7 +7,6 @@ dependencies {
     api(project(GradleModule.toReferenceName(GradleModule.CONTRACT_MODEL)))
 
     api(GradleDependency.TOOL_ALIBABA_TRANSMITTABLE_THREAD_LOCAL.withoutVersion)
-    api(GradleDependency.TOOL_BURNING_WAVE_CORE.withoutVersion)
     api(GradleDependency.TOOL_GOOGLE_GUAVA.withoutVersion)
     api(GradleDependency.TOOL_HUTOOL.withoutVersion)
     api(GradleDependency.TOOL_IO_VAVR.withoutVersion)
