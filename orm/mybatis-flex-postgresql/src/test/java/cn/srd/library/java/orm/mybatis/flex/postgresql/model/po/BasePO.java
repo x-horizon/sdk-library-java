b638ac2c8c6aeb1779725d14f9b5a87a28b8f3c2
jimmy <wjmwss@outlook.com>
2023-11-24 00:09:57 +0800

    - style: unit test；

--- orm/mybatis-flex-postgresql/src/test/java/cn/srd/library/java/orm/mybatis/flex/postgresql/model/po/BasePO.java
+++ orm/mybatis-flex-postgresql/src/test/java/cn/srd/library/java/orm/mybatis/flex/postgresql/model/po/BasePO.java
@@ -1,24 +1,26 @@
 package cn.srd.library.java.orm.mybatis.flex.postgresql.model.po;
 
+import cn.srd.library.java.orm.contract.model.base.PO;
 import cn.srd.library.java.tool.convert.jackson.deserializer.JacksonLongToLocalDateTimeDeserializer;
 import cn.srd.library.java.tool.convert.jackson.serializer.JacksonLocalDateTimeToLongSerializer;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.mybatisflex.annotation.Column;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import lombok.experimental.SuperBuilder;
 
 import java.io.Serial;
-import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @Accessors(chain = true)
 @SuperBuilder(toBuilder = true)
-public class BasePO implements Serializable {
+@EqualsAndHashCode(callSuper = true)
+public class BasePO extends PO {
 
     @Serial private static final long serialVersionUID = 2152316212528302390L;
 
