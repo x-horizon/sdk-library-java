d22147c63e9bde7483de70f571a942e81fd2f842
jimmy <wjmwss@outlook.com>
2023-11-23 17:05:43 +0800

    - style: reformat code；

--- tool/convert/all/src/test/java/cn/srd/library/java/tool/convert/all/mapstruct/converter/StudentMapstructConverter.java
+++ tool/convert/all/src/test/java/cn/srd/library/java/tool/convert/all/mapstruct/converter/StudentMapstructConverter.java
@@ -14,7 +14,6 @@ import cn.srd.library.java.tool.convert.mapstruct.utils.MapstructMappingManager;
 import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -25,31 +24,25 @@ public interface StudentMapstructConverter {
 
     StudentMapstructConverter INSTANCE = Mappers.getMapper(StudentMapstructConverter.class);
 
-    @Mappings({
-            @Mapping(source = "id", target = "studentId"),
-            @Mapping(source = "name", target = "studentName"),
-            @Mapping(source = "age", target = "studentAge", ignore = true),
-            @Mapping(source = "sex.number", target = "studentSexNumber"),
-            @Mapping(source = "sex", target = "studentSexName"),
-    })
+    @Mapping(source = "id", target = "studentId")
+    @Mapping(source = "name", target = "studentName")
+    @Mapping(source = "age", target = "studentAge", ignore = true)
+    @Mapping(source = "sex.number", target = "studentSexNumber")
+    @Mapping(source = "sex", target = "studentSexName")
     StudentVO toVO(StudentDO studentDO);
 
-    @Mappings({
-            @Mapping(source = "studentDO.id", target = "studentId"),
-            @Mapping(source = "gradeDO.name", target = "studentName"),
-            @Mapping(source = "studentDO.age", target = "studentAge", ignore = true),
-            @Mapping(source = "studentDO.sex.number", target = "studentSexNumber"),
-            @Mapping(source = "studentDO.sex", target = "studentSexName"),
-    })
+    @Mapping(source = "studentDO.id", target = "studentId")
+    @Mapping(source = "gradeDO.name", target = "studentName")
+    @Mapping(source = "studentDO.age", target = "studentAge", ignore = true)
+    @Mapping(source = "studentDO.sex.number", target = "studentSexNumber")
+    @Mapping(source = "studentDO.sex", target = "studentSexName")
     StudentVO toVO(StudentDO studentDO, GradeDO gradeDO);
 
-    @Mappings({
-            @Mapping(source = "gradeDO1.id", target = "studentId"),
-            @Mapping(source = "gradeDO2.name", target = "studentName"),
-            @Mapping(source = "studentDO.age", target = "studentAge", ignore = true),
-            @Mapping(source = "studentDO.sex.number", target = "studentSexNumber"),
-            @Mapping(source = "studentDO.sex", target = "studentSexName"),
-    })
+    @Mapping(source = "gradeDO1.id", target = "studentId")
+    @Mapping(source = "gradeDO2.name", target = "studentName")
+    @Mapping(source = "studentDO.age", target = "studentAge", ignore = true)
+    @Mapping(source = "studentDO.sex.number", target = "studentSexNumber")
+    @Mapping(source = "studentDO.sex", target = "studentSexName")
     StudentVO toVO(StudentDO studentDO, GradeDO gradeDO1, GradeDO gradeDO2);
 
     @IterableMapping(elementTargetType = StudentVO.class)
