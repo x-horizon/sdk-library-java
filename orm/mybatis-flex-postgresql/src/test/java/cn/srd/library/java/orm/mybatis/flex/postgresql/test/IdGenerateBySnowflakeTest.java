6154de1b9c885e1f972ee2372b32710d9d56d741
jimmy <wjmwss@outlook.com>
2023-11-27 20:36:52 +0800

    - style: unit test；

--- orm/mybatis-flex-postgresql/src/test/java/cn/srd/library/java/orm/mybatis/flex/postgresql/test/IdGenerateBySnowflakeTest.java
+++ orm/mybatis-flex-postgresql/src/test/java/cn/srd/library/java/orm/mybatis/flex/postgresql/test/IdGenerateBySnowflakeTest.java
@@ -11,8 +11,10 @@ import cn.srd.library.java.orm.mybatis.flex.base.property.PropertyConfig;
 import cn.srd.library.java.orm.mybatis.flex.postgresql.config.TestInsertListener;
 import cn.srd.library.java.orm.mybatis.flex.postgresql.config.TestUpdateListener;
 import cn.srd.library.java.orm.mybatis.flex.postgresql.dao.StudentTestIdSnowflakeDao;
+import cn.srd.library.java.orm.mybatis.flex.postgresql.model.po.StudentTestIdSnowflakePO;
 import cn.srd.library.java.tool.id.snowflake.EnableSnowflakeId;
 import cn.srd.library.java.tool.id.snowflake.SnowflakeIdEnvironment;
+import cn.srd.library.java.tool.lang.collection.Collections;
 import org.apache.ibatis.logging.nologging.NoLoggingImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,6 +22,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Map;
 
 @MapperScan("cn.srd.library.java.orm.mybatis.flex.postgresql.dao")
 @EnableSnowflakeId(environment = SnowflakeIdEnvironment.MULTIPLE_NODE)
@@ -42,9 +47,36 @@ class IdGenerateBySnowflakeTest {
 
     @Test
     void testIt() {
-        // studentTestIdSnowflakeDao.listByCondition(QueryWrapper.create()
-        //         .where(STUDENT_TEST_ID_SNOWFLAKE.ID.equalIfNotNull(1))
-        // );
+        // StudentTestIdSnowflakePO studentTestIdSnowflakePO1 = studentTestIdSnowflakeDao.save(StudentTestIdSnowflakePO.builder().build());
+        // Assertions.assertThrowsExactly(MyBatisSystemException.class, () -> studentTestIdSnowflakeDao.saveWithPK(StudentTestIdSnowflakePO.builder().build()));
+        // StudentTestIdSnowflakePO studentTestIdSnowflakePO3 = studentTestIdSnowflakeDao.saveWithPK(StudentTestIdSnowflakePO.builder().id(9999L).build());
+        //
+        // Map<Integer, StudentTestIdSnowflakePO> smallStudentTestIdSnowflakePOs = Collections.newHashMap();
+        // for (int i = 1; i < 60; i++) {
+        //     smallStudentTestIdSnowflakePOs.put(i, StudentTestIdSnowflakePO.builder().build());
+        // }
+        // List<StudentTestIdSnowflakePO> studentTestIdSnowflakePOs1 = studentTestIdSnowflakeDao.saveBatch(smallStudentTestIdSnowflakePOs.values());
+        //
+        // Map<Integer, StudentTestIdSnowflakePO> bigStudentTestIdSnowflakePOs = Collections.newHashMap();
+        // for (int i = 1; i < 300; i++) {
+        //     bigStudentTestIdSnowflakePOs.put(i, StudentTestIdSnowflakePO.builder().build());
+        // }
+        // List<StudentTestIdSnowflakePO> studentTestIdSnowflakePOs2 = studentTestIdSnowflakeDao.saveBatch(bigStudentTestIdSnowflakePOs.values());
+
+        Map<Integer, StudentTestIdSnowflakePO> smallStudentTestIdSnowflakeWithIdPOs = Collections.newHashMap();
+        for (int i = 1; i < 60; i++) {
+            Long id = (long) (10000 + i);
+            smallStudentTestIdSnowflakeWithIdPOs.put(i, StudentTestIdSnowflakePO.builder().id(id).build());
+        }
+        List<StudentTestIdSnowflakePO> studentTestIdSnowflakePOs3 = studentTestIdSnowflakeDao.saveBatch(smallStudentTestIdSnowflakeWithIdPOs.values());
+
+        Map<Integer, StudentTestIdSnowflakePO> bigStudentTestIdSnowflakeWithIdPOs = Collections.newHashMap();
+        for (int i = 1; i < 300; i++) {
+            Long id = (long) (20000 + i);
+            bigStudentTestIdSnowflakeWithIdPOs.put(i, StudentTestIdSnowflakePO.builder().id(id).build());
+        }
+        List<StudentTestIdSnowflakePO> studentTestIdSnowflakePOs4 = studentTestIdSnowflakeDao.saveBatch(bigStudentTestIdSnowflakeWithIdPOs.values());
+
         studentTestIdSnowflakeDao.listAll();
         studentTestIdSnowflakeDao.listAll();
         studentTestIdSnowflakeDao.listAll();
