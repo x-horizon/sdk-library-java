0d2a1f8198ee082683ed280514226f87e8322dec
jimmy <wjmwss@outlook.com>
2023-12-04 21:00:31 +0800

    - optimize: 增强 sql 查询性能，若页码大于 1 时，由客户端提供数据总量；

--- orm/contract/src/main/java/cn/srd/library/java/orm/contract/model/page/PageParam.java
+++ orm/contract/src/main/java/cn/srd/library/java/orm/contract/model/page/PageParam.java
@@ -7,12 +7,14 @@ package cn.srd.library.java.orm.contract.model.page;
 import cn.srd.library.java.contract.constant.page.PageConstant;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.group.GroupSequenceProvider;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -28,6 +30,7 @@ import java.io.Serializable;
 @NoArgsConstructor
 @Accessors(chain = true)
 @SuperBuilder(toBuilder = true)
+@GroupSequenceProvider(PageParamValidationGroup.class)
 public class PageParam implements Serializable {
 
     @Serial private static final long serialVersionUID = -2956893884714618641L;
@@ -41,5 +44,9 @@ public class PageParam implements Serializable {
     @Range(min = 1, max = 100, message = "the range of record number per page is [1, 100]")
     @Builder.Default
     private Integer pageSize = PageConstant.DEFAULT_PAGE_SIZE;
+
+    @Schema(description = "total record number, must > 0 if page index > 1", example = "999")
+    @NotNull(message = "the total record number must > 0 if page index > 1", groups = PageParamValidationGroup.TotalRecordNumberValidator.class)
+    private Long total;
 
 }
---
+++ orm/contract/src/main/java/cn/srd/library/java/orm/contract/model/page/PageParamValidationGroup.java
@@ -0,0 +1,36 @@
+// Copyright (C) 2021-2023 thinkingto.com Ltd. All rights reserved.
+// Use of this source code is governed by SRD.
+// license that can be found in the LICENSE file.
+
+package cn.srd.library.java.orm.contract.model.page;
+
+import cn.srd.library.java.tool.lang.collection.Collections;
+import cn.srd.library.java.tool.lang.object.Nil;
+import org.hibernate.validator.spi.group.DefaultGroupSequenceProvider;
+
+import java.util.List;
+
+/**
+ * the page param validation group
+ *
+ * @author wjm
+ * @since 2023-12-04 20:24
+ */
+public class PageParamValidationGroup implements DefaultGroupSequenceProvider<PageParam> {
+
+    @Override
+    public List<Class<?>> getValidationGroups(PageParam pageParam) {
+        // the default validation group must be added
+        List<Class<?>> defaultValidationGroups = Collections.ofArrayList(PageParam.class);
+        // must ensure the instance being validated is not null
+        if (Nil.isNotNull(pageParam) && pageParam.getPageIndex() > 1 && Nil.isNull(pageParam.getTotal())) {
+            defaultValidationGroups.add(TotalRecordNumberValidator.class);
+        }
+        return defaultValidationGroups;
+    }
+
+    public interface TotalRecordNumberValidator {
+
+    }
+
+}
