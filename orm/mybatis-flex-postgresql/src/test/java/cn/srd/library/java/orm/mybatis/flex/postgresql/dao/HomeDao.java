6b98be2436168a460cf65c5908a521d772c7a3bd
jimmy <wjmwss@outlook.com>
2024-04-18 22:16:43 +0800

    - feature: 完善 library-orm-mybatis-flex 单元测试；

--- orm/mybatis-flex-postgresql/src/test/java/cn/srd/library/java/orm/mybatis/flex/postgresql/dao/HomeDao.java
+++ orm/mybatis-flex-postgresql/src/test/java/cn/srd/library/java/orm/mybatis/flex/postgresql/dao/HomeDao.java
@@ -1,6 +1,5 @@
 package cn.srd.library.java.orm.mybatis.flex.postgresql.dao;
 
-import cn.srd.library.java.orm.mybatis.flex.base.dao.GenericDao;
 import cn.srd.library.java.orm.mybatis.flex.postgresql.model.po.HomePO;
 import org.springframework.stereotype.Repository;
 
--- orm/mybatis-flex-postgresql/src/test/java/cn/srd/library/java/orm/mybatis/flex/postgresql/dao/PeopleDao.java
+++ orm/mybatis-flex-postgresql/src/test/java/cn/srd/library/java/orm/mybatis/flex/postgresql/dao/PeopleDao.java
@@ -1,6 +1,5 @@
 package cn.srd.library.java.orm.mybatis.flex.postgresql.dao;
 
-import cn.srd.library.java.orm.mybatis.flex.base.dao.GenericDao;
 import cn.srd.library.java.orm.mybatis.flex.postgresql.model.po.PeoplePO;
 import org.springframework.stereotype.Repository;
 
