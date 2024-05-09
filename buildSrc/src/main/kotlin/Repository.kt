83631af1d168c4636c29007a009c4efbc4f90028
jimmy <wjmwss@outlook.com>
2024-05-09 22:53:12 +0800

    - style: 修改本地环境 nexus 的端口号；

--- buildSrc/src/main/kotlin/Repository.kt
+++ buildSrc/src/main/kotlin/Repository.kt
@@ -14,11 +14,8 @@ object GradleRepository {
     val nexusUrl = GradleConfig.activeEnvironment.handler.getNexusUrl()
     const val NEXUS_USERNAME = "srd-library-java"
     const val NEXUS_PASSWORD = "kVPH8d9hktREbQ"
-    const val NEXUS_LOCALHOST_URL = "http://localhost:8081/repository/srd-library-java/"
+    const val NEXUS_LOCALHOST_URL = "http://localhost:10015/repository/srd-library-java/"
     const val NEXUS_DEVELOPMENT_URL = "http://192.168.10.91:32122/repository/srd-library-java/"
     const val NEXUS_PRODUCTION_URL = "http://192.168.10.86:32208/repository/srd-library-java/"
 
 }
-
-
-
