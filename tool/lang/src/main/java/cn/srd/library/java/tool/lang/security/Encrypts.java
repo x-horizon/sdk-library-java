64622a1c942128de0c4a3c93680cd5dc126f7963
jimmy <wjmwss@outlook.com>
2024-05-14 22:38:28 +0800

    - feature: 更多常用工具；

--- tool/lang/src/main/java/cn/srd/library/java/tool/lang/security/Encrypts.java
+++ tool/lang/src/main/java/cn/srd/library/java/tool/lang/security/Encrypts.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by SRD.
 // license that can be found in the LICENSE file.
 
-package cn.srd.library.java.tool.lang.secutiry;
+package cn.srd.library.java.tool.lang.security;
 
 import cn.hutool.crypto.SecureUtil;
 import lombok.AccessLevel;
