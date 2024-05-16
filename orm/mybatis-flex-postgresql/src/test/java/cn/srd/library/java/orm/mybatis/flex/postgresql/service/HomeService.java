5d44798195c38643b29677c7da8b9043d952e545
jimmy <wjmwss@outlook.com>
2024-05-16 16:22:56 +0800

    - feature: 完善 library-orm-mybatis-flex 相关功能；

--- orm/mybatis-flex-postgresql/src/test/java/cn/srd/library/java/orm/mybatis/flex/postgresql/service/HomeService.java
+++ orm/mybatis-flex-postgresql/src/test/java/cn/srd/library/java/orm/mybatis/flex/postgresql/service/HomeService.java
@@ -7,16 +7,17 @@ package cn.srd.library.java.orm.mybatis.flex.postgresql.service;
 import cn.srd.library.java.orm.mybatis.flex.postgresql.model.po.HomePO;
 import cn.srd.library.java.orm.mybatis.flex.postgresql.model.vo.HomeVO;
 import cn.srd.library.java.orm.mybatis.flex.postgresql.repository.HomeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 /**
  * @author wjm
  * @since 2024-04-16 15:26
  */
+@AllArgsConstructor
 @Service
 public class HomeService extends GenericService<HomePO, HomeVO, HomeRepository> {
 
-    @Autowired private HomeRepository homeRepository;
+    private final HomeRepository homeRepository;
 
 }
