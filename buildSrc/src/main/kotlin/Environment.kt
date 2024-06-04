ebd1050d72f643b8aa0e2092c678ec6c84a4ff6e
jimmy <wjmwss@outlook.com>
2024-06-04 09:15:25 +0800

    - style: 移除无用代码；

--- buildSrc/src/main/kotlin/Environment.kt
+++ buildSrc/src/main/kotlin/Environment.kt
@@ -6,7 +6,6 @@ enum class Environment(val code: String, val handler: EnvironmentHandler) {
 
     LOCALHOST("localhost", EnvironmentLocalhostHandler),
     DEVELOPMENT("development", EnvironmentDevelopmentHandler),
-    PRODUCTION("production", EnvironmentProductionHandler),
 
     ;
 
