77db09b429eb516fbf6dfab528336cb8806cb8bb
jimmy <wjmwss@outlook.com>
2024-06-04 09:13:59 +0800

    - style: 更新 license；

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,4 +1,4 @@
-// Copyright (C) 2021-2023 thinkingto.com Ltd. All rights reserved.
+// Copyright (C) 2021-2026 thinkingto.com Ltd. All rights reserved.
 // Use of this source code is governed by SRD.
 // license that can be found in the LICENSE file.
 
@@ -14,6 +14,3 @@ object GradlePlugin {
     const val CHECK_STYLE_CONFIG_PATH = "/config/checkstyle/checkstyle.xml"
 
 }
-
-
-
--- orm/contract/build.gradle.kts
+++ orm/contract/build.gradle.kts
@@ -1,4 +1,4 @@
-// Copyright (C) 2021-2023 thinkingto.com Ltd. All rights reserved.
+// Copyright (C) 2021-2026 thinkingto.com Ltd. All rights reserved.
 // Use of this source code is governed by SRD.
 // license that can be found in the LICENSE file.
 
@@ -9,7 +9,7 @@ dependencies {
     api(project(GradleModule.toReferenceName(GradleModule.TOOL_ENUMS)))
     api(project(GradleModule.toReferenceName(GradleModule.TOOL_LANG)))
     api(project(GradleModule.toReferenceName(GradleModule.TOOL_SPRING_CONTRACT)))
-    
+
     api(GradleDependency.TOOL_VALIDATION_HIBERNATE.withoutVersion)
 
     annotationProcessor(project(GradleModule.toReferenceName(GradleModule.PLUGGABLE_ANNOTATION_API_PROCESSOR_LOMBOK_MAPSTRUCT_BINDING)))
