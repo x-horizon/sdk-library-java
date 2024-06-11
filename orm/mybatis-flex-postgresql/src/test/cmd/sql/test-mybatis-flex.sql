8107dc974225b3766c44ce16a175b4e3854a733a
jimmy <wjmwss@outlook.com>
2024-06-11 16:18:46 +0800

    - fix：测试用例补充缺少的字段；

--- orm/mybatis-flex-postgresql/src/test/cmd/sql/test-mybatis-flex.sql
+++ orm/mybatis-flex-postgresql/src/test/cmd/sql/test-mybatis-flex.sql
@@ -7,35 +7,39 @@ CREATE EXTENSION IF NOT EXISTS address_standardizer;
 
 CREATE TABLE home
 (
-    id          BIGINT                                       NOT NULL,
-    name        VARCHAR(64)                    DEFAULT ''    NOT NULL,
-    remark      VARCHAR(255)                   DEFAULT ''    NOT NULL,
-    creator_id  BIGINT                         DEFAULT 0     NOT NULL,
-    updater_id  BIGINT                         DEFAULT 0     NOT NULL,
-    create_time TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
-    update_time TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
-    delete_time TIMESTAMP(6) WITHOUT TIME ZONE,
+    id           BIGINT                                       NOT NULL,
+    name         VARCHAR(64)                    DEFAULT ''    NOT NULL,
+    remark       VARCHAR(255)                   DEFAULT ''    NOT NULL,
+    creator_id   BIGINT                         DEFAULT 0     NOT NULL,
+    creator_name VARCHAR(64)                    DEFAULT ''    NOT NULL,
+    updater_id   BIGINT                         DEFAULT 0     NOT NULL,
+    updater_name VARCHAR(64)                    DEFAULT ''    NOT NULL,
+    create_time  TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
+    update_time  TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
+    delete_time  TIMESTAMP(6) WITHOUT TIME ZONE,
     PRIMARY KEY (id)
 );
 
 
 CREATE TABLE people
 (
-    id          BIGINT                                       NOT NULL,
-    home_id     BIGINT                         DEFAULT 0     NOT NULL,
-    name1       VARCHAR(64)                    DEFAULT ''    NOT NULL,
-    name2       VARCHAR(64)                    DEFAULT ''    NOT NULL,
-    name3       VARCHAR(64)                    DEFAULT ''    NOT NULL,
-    name4       VARCHAR(64)                    DEFAULT ''    NOT NULL,
-    name5       VARCHAR(64)                    DEFAULT ''    NOT NULL,
-    name6       VARCHAR(64)                    DEFAULT ''    NOT NULL,
-    name7       VARCHAR(64)                    DEFAULT ''    NOT NULL,
-    version     BIGINT                         DEFAULT 0     NOT NULL,
-    remark      VARCHAR(255)                   DEFAULT ''    NOT NULL,
-    creator_id  BIGINT                         DEFAULT 0     NOT NULL,
-    updater_id  BIGINT                         DEFAULT 0     NOT NULL,
-    create_time TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
-    update_time TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
-    delete_time TIMESTAMP(6) WITHOUT TIME ZONE,
+    id           BIGINT                                       NOT NULL,
+    home_id      BIGINT                         DEFAULT 0     NOT NULL,
+    name1        VARCHAR(64)                    DEFAULT ''    NOT NULL,
+    name2        VARCHAR(64)                    DEFAULT ''    NOT NULL,
+    name3        VARCHAR(64)                    DEFAULT ''    NOT NULL,
+    name4        VARCHAR(64)                    DEFAULT ''    NOT NULL,
+    name5        VARCHAR(64)                    DEFAULT ''    NOT NULL,
+    name6        VARCHAR(64)                    DEFAULT ''    NOT NULL,
+    name7        VARCHAR(64)                    DEFAULT ''    NOT NULL,
+    version      BIGINT                         DEFAULT 0     NOT NULL,
+    remark       VARCHAR(255)                   DEFAULT ''    NOT NULL,
+    creator_id   BIGINT                         DEFAULT 0     NOT NULL,
+    creator_name VARCHAR(64)                    DEFAULT ''    NOT NULL,
+    updater_id   BIGINT                         DEFAULT 0     NOT NULL,
+    updater_name VARCHAR(64)                    DEFAULT ''    NOT NULL,
+    create_time  TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
+    update_time  TIMESTAMP(6) WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
+    delete_time  TIMESTAMP(6) WITHOUT TIME ZONE,
     PRIMARY KEY (id)
 );
