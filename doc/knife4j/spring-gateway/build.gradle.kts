3082ce2f7aabb863b27d90926fbd3b1006eadcba
jimmy <wjmwss@outlook.com>
2024-07-06 23:19:55 +0800

    - style: 移除冲突的依赖；

--- doc/knife4j/spring-gateway/build.gradle.kts
+++ doc/knife4j/spring-gateway/build.gradle.kts
@@ -4,7 +4,6 @@
 
 dependencies {
     api(project(GradleModule.toReferenceName(GradleModule.DOC_KNIFE4J_CONTRACT)))
-    api(project(GradleModule.toReferenceName(GradleModule.DOC_KNIFE4J_SPRING_WEBFLUX)))
 
     api(GradleDependency.DOC_XIAOYMIN_KNIFE4J_SPRING_GATEWAY.withoutVersion)
 }
