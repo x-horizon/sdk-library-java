f7ae67c6d8a550879d9e5d668429b3d8ef6d2840
wjmwss <wjmwss@outlook.com>
2025-01-06 20:18:03 +0800

    - style：优化依赖；

--- message-engine/client/contract/build.gradle.kts
+++ message-engine/client/contract/build.gradle.kts
@@ -1,7 +1,6 @@
 dependencies {
     api(project(GradleModule.toReferenceName(GradleModule.MESSAGE_ENGINE_CONTRACT)))
     api(project(GradleModule.toReferenceName(GradleModule.TOOL_ID_SNOWFLAKE)))
-    api(project(GradleModule.toReferenceName(GradleModule.TOOL_ID_UUID)))
 
     api(GradleDependency.MESSAGE_SPRING_INTEGRATION_STREAM.withoutVersion)
 }
--- message-engine/contract/build.gradle.kts
+++ message-engine/contract/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     api(project(GradleModule.toReferenceName(GradleModule.TOOL_ENUMS)))
+    api(project(GradleModule.toReferenceName(GradleModule.TOOL_ID_UUID)))
     api(project(GradleModule.toReferenceName(GradleModule.TOOL_LANG)))
     api(project(GradleModule.toReferenceName(GradleModule.TOOL_SPRING_CONTRACT)))
     api(project(GradleModule.toReferenceName(GradleModule.TOOL_CONVERT_API)))
