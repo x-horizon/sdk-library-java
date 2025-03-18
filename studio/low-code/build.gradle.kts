28e23facde5afd864e9d804b5c141da40575f7bd
jimmy <wjmwss@outlook.com>
2025-03-19 05:43:18 +0800

    - style：移除无用依赖；

--- studio/low-code/build.gradle.kts
+++ studio/low-code/build.gradle.kts
@@ -1,8 +1,6 @@
 dependencies {
     testImplementation(project(GradleModule.JDBC_POSTGRESQL))
-
     testImplementation(project(GradleModule.DOC_KNIFE4J_SPRING_WEBMVC))
-
     testImplementation(project(GradleModule.TOOL_ENUMS))
     testImplementation(project(GradleModule.TOOL_GEOMETRY))
     testImplementation(project(GradleModule.TOOL_JDK))
@@ -14,12 +12,8 @@ dependencies {
     testImplementation(project(GradleModule.TOOL_SPRING_WEBMVC))
     testImplementation(project(GradleModule.TOOL_VALIDATION_JAKARTA))
     testImplementation(project(GradleModule.TOOL_VALIDATION_SPRING))
-
     testImplementation(project(GradleModule.ORM_MYBATIS_FLEX_POSTGRESQL))
 
-    annotationProcessor(project(GradleModule.PLUGGABLE_ANNOTATION_API_PROCESSOR_LOMBOK_MAPSTRUCT_BINDING))
-    annotationProcessor(project(GradleModule.PLUGGABLE_ANNOTATION_API_PROCESSOR_MAPSTRUCT_PLUS))
-    annotationProcessor(project(GradleModule.PLUGGABLE_ANNOTATION_API_PROCESSOR_MYBATIS_FLEX))
     testAnnotationProcessor(project(GradleModule.PLUGGABLE_ANNOTATION_API_PROCESSOR_LOMBOK_MAPSTRUCT_BINDING))
     testAnnotationProcessor(project(GradleModule.PLUGGABLE_ANNOTATION_API_PROCESSOR_MAPSTRUCT_PLUS))
     testAnnotationProcessor(project(GradleModule.PLUGGABLE_ANNOTATION_API_PROCESSOR_MYBATIS_FLEX))
