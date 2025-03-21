af4b1f8766eb76889a1fe0e25c32bd6fbd3c33cc
jimmy <wjmwss@outlook.com>
2025-03-21 16:02:40 +0800

    - style：增加单元测试示例；

--- tool/convert/jackson/build.gradle.kts
+++ tool/convert/jackson/build.gradle.kts
@@ -1,3 +1,6 @@
 dependencies {
     api(project(GradleModule.TOOL_SERIALIZATION_JACKSON))
+
+    testAnnotationProcessor(project(GradleModule.PLUGGABLE_ANNOTATION_API_PROCESSOR_LOMBOK_MAPSTRUCT_BINDING))
+    testAnnotationProcessor(project(GradleModule.PLUGGABLE_ANNOTATION_API_PROCESSOR_MAPSTRUCT_PLUS))
 }
