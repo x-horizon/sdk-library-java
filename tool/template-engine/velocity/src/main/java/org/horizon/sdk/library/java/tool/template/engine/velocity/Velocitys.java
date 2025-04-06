0b1d38e9710d193b51a7725cfa0aca2e2f2cd880
jimmy <wjmwss@outlook.com>
2025-04-06 17:08:43 +0800

    - feature：更多常用工具；

---
+++ tool/template-engine/velocity/src/main/java/org/horizon/sdk/library/java/tool/template/engine/velocity/Velocitys.java
@@ -0,0 +1,35 @@
+package org.horizon.sdk.library.java.tool.template.engine.velocity;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+
+import java.io.StringWriter;
+
+/**
+ * toolkit for velocity
+ *
+ * @author wjm
+ * @since 2025-04-06 17:05
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Velocitys {
+
+    private static final String DEFAULT_VELOCITY_LOG_NAME = "defaultVelocityLog";
+
+    static {
+        Velocity.init();
+    }
+
+    public static String evaluate(VelocityContext velocityContext, String template) {
+        return evaluate(velocityContext, DEFAULT_VELOCITY_LOG_NAME, template);
+    }
+
+    public static String evaluate(VelocityContext velocityContext, String logName, String template) {
+        StringWriter writer = new StringWriter();
+        Velocity.evaluate(velocityContext, writer, logName, template);
+        return writer.toString();
+    }
+
+}
