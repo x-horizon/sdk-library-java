95bdd5e70f5bfd1abf5246bd83f5e77bf6f35df0
jimmy <wjmwss@outlook.com>
2025-04-13 20:14:19 +0800

    - feature：更多常用工具；

--- registration/nacos/build.gradle.kts
+++ registration/nacos/build.gradle.kts
@@ -1,5 +1,8 @@
 dependencies {
     api(project(GradleModule.TOOL_LANG))
+    api(project(GradleModule.TOOL_SPRING_CONTRACT))
+    api(project(GradleModule.TOOL_CONVERT_API))
+    api(project(GradleModule.TOOL_CONVERT_JACKSON))
 
     api(GradleDependency.LOADBALANCER_SPRING_CLOUD.withoutVersion)
     api(GradleDependency.REGISTRATION_SPRING_CLOUD_ALIBABA_NACOS_CONFIG.withoutVersion)
