49b87e8e45a30a01478ce585b4c29174a6d1d604
jimmy <wjmwss@outlook.com>
2025-04-16 23:11:36 +0800

    - optimize：支持生成父类字段、父类内部类；

--- pluggable-annotation-api/processor/field-metadata/src/main/java/org/horizon/sdk/library/java/pluggable/annotation/api/processor/field/metadata/processor/FieldMetadataProcessor.java
+++ pluggable-annotation-api/processor/field-metadata/src/main/java/org/horizon/sdk/library/java/pluggable/annotation/api/processor/field/metadata/processor/FieldMetadataProcessor.java
@@ -21,9 +21,13 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 /**
  * @author wjm
@@ -42,6 +46,10 @@ public class FieldMetadataProcessor extends AbstractProcessor {
     );
 
     private static final String KNIFE4J_SCHEMA_CLASS_NAME = "io.swagger.v3.oas.annotations.media.Schema";
+
+    private static final List<String> LOMBOK_INNER_CLASS_NAME_SUFFIXES = Collections.ofImmutableList("Builder", "BuilderImpl");
+
+    private static final Predicate<String> LOMBOK_INNER_CLASS_NAME_FILTER = className -> LOMBOK_INNER_CLASS_NAME_SUFFIXES.stream().noneMatch(className::endsWith);
 
     private static final String PLACEHOLDER = "$S";
 
@@ -81,13 +89,14 @@ public class FieldMetadataProcessor extends AbstractProcessor {
         roundEnvironment.getElementsAnnotatedWith(FieldMetadata.class)
                 .stream()
                 .filter(TypeElement.class::isInstance)
+                .map(TypeElement.class::cast)
                 .forEach(this::buildEntityFieldClass);
         return true;
     }
 
     @SneakyThrows
-    private void buildEntityFieldClass(Element element) {
-        String entityFieldClassName = getEntityFieldClassName(element);
+    private void buildEntityFieldClass(TypeElement typeElement) {
+        String entityFieldClassName = getEntityFieldClassName(typeElement);
         TypeSpec.Builder entityFieldClassBuilder = TypeSpec.classBuilder(entityFieldClassName)
                 .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
                 .addSuperinterface(EntityField.class)
@@ -96,79 +105,99 @@ public class FieldMetadataProcessor extends AbstractProcessor {
                         .build()
                 );
 
-        processClassElements(element, entityFieldClassBuilder);
+        // build current class fields and inherited class fields
+        Stream.iterate(typeElement, Nil::isNotNull, this::getSuperClass)
+                .flatMap(elem -> getValidFieldElement(elem).stream())
+                .forEach(field -> processFieldMetadataClass(entityFieldClassBuilder, field));
 
-        String packageName = Strings.subBefore(this.elementUtil.getPackageOf(element).toString(), SymbolConstant.DOT) + ENTITY_FIELD_CLASS_PACKAGE_NAME_SUFFIX;
+        // build current class inner classes and inherited class inner classes
+        Stream.iterate(typeElement, Nil::isNotNull, this::getSuperClass)
+                .flatMap(elem -> getValidClassElement(elem).stream())
+                .forEach(innerClassElement -> processInnerFieldMetadataClass(entityFieldClassBuilder, innerClassElement));
+
+        String packageName = Strings.subBefore(this.elementUtil.getPackageOf(typeElement).toString(), SymbolConstant.DOT) + ENTITY_FIELD_CLASS_PACKAGE_NAME_SUFFIX;
         JavaFile.builder(packageName, entityFieldClassBuilder.build())
                 .build()
                 .writeTo(this.processingEnv.getFiler());
     }
 
-    private void processClassElements(Element element, TypeSpec.Builder classBuilder) {
+    private void processFieldMetadataClassElement(Element element, TypeSpec.Builder classBuilder) {
         // handle outer class field
-        element.getEnclosedElements().stream()
-                .filter(enclosedElement -> enclosedElement.getKind().isField())
-                .filter(field -> Collections.notContainsAll(field.getModifiers(), Modifier.STATIC, Modifier.FINAL))
-                .forEach(field -> {
-                    String fieldName = field.getSimpleName().toString();
-                    String fieldComment = this.getFieldComment(field);
-                    String className = Strings.upperFirst(field.getSimpleName().toString());
-                    String underlineUpperCaseEntityFieldClassFieldNameName = Strings.underlineUpperCase(ENTITY_FIELD_CLASS_FIELD_NAME_NAME);
-                    String underlineUpperCaseEntityFieldClassFieldCommentName = Strings.underlineUpperCase(ENTITY_FIELD_CLASS_FIELD_COMMENT_NAME);
-                    assert underlineUpperCaseEntityFieldClassFieldNameName != null;
-                    assert underlineUpperCaseEntityFieldClassFieldCommentName != null;
-                    classBuilder.addType(TypeSpec.classBuilder(className)
-                            .addModifiers(Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL)
-                            .addMethod(MethodSpec.constructorBuilder()
-                                    .addModifiers(Modifier.PRIVATE)
-                                    .build()
-                            )
-                            .addField(FieldSpec.builder(String.class, underlineUpperCaseEntityFieldClassFieldNameName)
-                                    .addModifiers(Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL)
-                                    .initializer(PLACEHOLDER, fieldName)
-                                    .build()
-                            )
-                            .addField(FieldSpec.builder(String.class, underlineUpperCaseEntityFieldClassFieldCommentName)
-                                    .addModifiers(Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL)
-                                    .initializer(PLACEHOLDER, fieldComment)
-                                    .build()
-                            )
-                            .addMethod(MethodSpec.methodBuilder(getMethodName(ENTITY_FIELD_CLASS_FIELD_NAME_NAME))
-                                    .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
-                                    .addCode(CodeBlock.of(CODE_RETURN_TEMPLATE, underlineUpperCaseEntityFieldClassFieldNameName))
-                                    .returns(String.class)
-                                    .build()
-                            )
-                            .addMethod(MethodSpec.methodBuilder(getMethodName(ENTITY_FIELD_CLASS_FIELD_COMMENT_NAME))
-                                    .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
-                                    .addCode(CodeBlock.of(CODE_RETURN_TEMPLATE, underlineUpperCaseEntityFieldClassFieldCommentName))
-                                    .returns(String.class)
-                                    .build()
-                            )
-                            .build()
-                    );
-                });
-
+        getValidFieldElement(element).forEach(fieldElement -> processFieldMetadataClass(classBuilder, fieldElement));
         // handle inner class
-        element.getEnclosedElements().stream()
-                .filter(enclosedElement -> enclosedElement.getKind() == ElementKind.CLASS)
-                .forEach(innerClass -> {
-                    String innerEntityFieldClassName = getEntityFieldClassName(innerClass);
-                    TypeSpec.Builder innerClassBuilder = TypeSpec.classBuilder(innerEntityFieldClassName)
-                            .addModifiers(Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL)
-                            .addMethod(MethodSpec.constructorBuilder()
-                                    .addModifiers(Modifier.PRIVATE)
-                                    .build()
-                            );
-                    processClassElements(innerClass, innerClassBuilder);
-                    classBuilder.addType(innerClassBuilder.build());
-                });
+        getValidClassElement(element).forEach(innerClassElement -> processInnerFieldMetadataClass(classBuilder, innerClassElement));
     }
 
-    private String getFieldComment(Element field) {
-        String fieldComment = Strings.removeBlank(this.elementUtil.getDocComment(field));
+    private void processFieldMetadataClass(TypeSpec.Builder classBuilder, Element fieldElement) {
+        String fieldName = fieldElement.getSimpleName().toString();
+        String fieldComment = this.getFieldComment(fieldElement);
+        String className = Strings.upperFirst(fieldElement.getSimpleName().toString());
+        String underlineUpperCaseEntityFieldClassFieldNameName = Strings.underlineUpperCase(ENTITY_FIELD_CLASS_FIELD_NAME_NAME);
+        String underlineUpperCaseEntityFieldClassFieldCommentName = Strings.underlineUpperCase(ENTITY_FIELD_CLASS_FIELD_COMMENT_NAME);
+        assert underlineUpperCaseEntityFieldClassFieldNameName != null;
+        assert underlineUpperCaseEntityFieldClassFieldCommentName != null;
+        classBuilder.addType(TypeSpec.classBuilder(className)
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL)
+                .addMethod(MethodSpec.constructorBuilder()
+                        .addModifiers(Modifier.PRIVATE)
+                        .build()
+                )
+                .addField(FieldSpec.builder(String.class, underlineUpperCaseEntityFieldClassFieldNameName)
+                        .addModifiers(Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL)
+                        .initializer(PLACEHOLDER, fieldName)
+                        .build()
+                )
+                .addField(FieldSpec.builder(String.class, underlineUpperCaseEntityFieldClassFieldCommentName)
+                        .addModifiers(Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL)
+                        .initializer(PLACEHOLDER, fieldComment)
+                        .build()
+                )
+                .addMethod(MethodSpec.methodBuilder(getMethodName(ENTITY_FIELD_CLASS_FIELD_NAME_NAME))
+                        .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                        .addCode(CodeBlock.of(CODE_RETURN_TEMPLATE, underlineUpperCaseEntityFieldClassFieldNameName))
+                        .returns(String.class)
+                        .build()
+                )
+                .addMethod(MethodSpec.methodBuilder(getMethodName(ENTITY_FIELD_CLASS_FIELD_COMMENT_NAME))
+                        .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                        .addCode(CodeBlock.of(CODE_RETURN_TEMPLATE, underlineUpperCaseEntityFieldClassFieldCommentName))
+                        .returns(String.class)
+                        .build()
+                )
+                .build()
+        );
+    }
+
+    private void processInnerFieldMetadataClass(TypeSpec.Builder classBuilder, Element innerClassElement) {
+        String innerEntityFieldClassName = getEntityFieldClassName(innerClassElement);
+        TypeSpec.Builder innerClassBuilder = TypeSpec.classBuilder(innerEntityFieldClassName)
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL)
+                .addMethod(MethodSpec.constructorBuilder()
+                        .addModifiers(Modifier.PRIVATE)
+                        .build()
+                );
+        processFieldMetadataClassElement(innerClassElement, innerClassBuilder);
+        classBuilder.addType(innerClassBuilder.build());
+    }
+
+    private List<? extends Element> getValidClassElement(Element element) {
+        return element.getEnclosedElements().stream()
+                .filter(enclosedElement -> enclosedElement.getKind() == ElementKind.CLASS)
+                .filter(classElement -> LOMBOK_INNER_CLASS_NAME_FILTER.test(classElement.getSimpleName().toString()))
+                .toList();
+    }
+
+    private List<? extends Element> getValidFieldElement(Element element) {
+        return element.getEnclosedElements().stream()
+                .filter(enclosedElement -> enclosedElement.getKind() == ElementKind.FIELD)
+                .filter(fieldElement -> Collections.notContainsAny(fieldElement.getModifiers(), Modifier.STATIC, Modifier.FINAL, Modifier.TRANSIENT))
+                .toList();
+    }
+
+    private String getFieldComment(Element fieldElement) {
+        String fieldComment = Strings.removeBlank(this.elementUtil.getDocComment(fieldElement));
         if (Nil.isNull(fieldComment) && ClassUtil.isClassExists(KNIFE4J_SCHEMA_CLASS_NAME, null)) {
-            Schema schema = field.getAnnotation(Schema.class);
+            Schema schema = fieldElement.getAnnotation(Schema.class);
             fieldComment = (Nil.isNull(schema)) ? SymbolConstant.EMPTY : schema.description();
         }
         fieldComment = Nil.isBlank(fieldComment) ? SymbolConstant.EMPTY : fieldComment;
@@ -179,8 +208,27 @@ public class FieldMetadataProcessor extends AbstractProcessor {
         return METHOD_GET_PREFIX + Strings.upperFirst(fieldName);
     }
 
-    private String getEntityFieldClassName(Element typeElement) {
-        return Strings.removeIfAnyEndWith(typeElement.getSimpleName(), NEED_TO_REMOVE_CLASS_NAME_SUFFIXES) + ENTITY_FIELD_CLASS_NAME_SUFFIX;
+    private String getEntityFieldClassName(Element element) {
+        return Strings.removeIfAnyEndWith(element.getSimpleName(), NEED_TO_REMOVE_CLASS_NAME_SUFFIXES) + ENTITY_FIELD_CLASS_NAME_SUFFIX;
+    }
+
+    private TypeElement getSuperClass(TypeElement typeElement) {
+        TypeMirror superClassMirror = typeElement.getSuperclass();
+        if (TypeKind.DECLARED == superClassMirror.getKind()) {
+            Element superElement = this.processingEnv.getTypeUtils().asElement(superClassMirror);
+            if (superElement instanceof TypeElement superTypeElement && isNotObjectClass(superTypeElement)) {
+                return superTypeElement;
+            }
+        }
+        return null;
+    }
+
+    private boolean isObjectClass(TypeElement typeElement) {
+        return typeElement.getQualifiedName().toString().equals(Object.class.getName());
+    }
+
+    private boolean isNotObjectClass(TypeElement typeElement) {
+        return !isObjectClass(typeElement);
     }
 
 }
