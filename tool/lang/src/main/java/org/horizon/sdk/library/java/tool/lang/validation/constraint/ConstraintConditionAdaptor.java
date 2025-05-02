3264e8029bb818fbe2640cd4e801009c5341dbfc
wjmwss <wjmwss@outlook.com>
2025-05-02 18:31:48 +0800

    - fix：修复未序列化进行克隆的问题；

--- tool/lang/src/main/java/org/horizon/sdk/library/java/tool/lang/validation/constraint/ConstraintConditionAdaptor.java
+++ tool/lang/src/main/java/org/horizon/sdk/library/java/tool/lang/validation/constraint/ConstraintConditionAdaptor.java
@@ -1,5 +1,6 @@
 package org.horizon.sdk.library.java.tool.lang.validation.constraint;
 
+import org.horizon.sdk.library.java.tool.lang.functional.SerializableBiPredicate;
 import org.horizon.sdk.library.java.tool.lang.validation.support.ValidationGroup;
 
 import java.util.function.BiPredicate;
@@ -18,6 +19,6 @@ import java.util.function.BiPredicate;
  * @since 2025-04-21 21:36
  */
 @FunctionalInterface
-public interface ConstraintConditionAdaptor<T> extends BiPredicate<T, ValidationGroup[]> {
+public interface ConstraintConditionAdaptor<T> extends SerializableBiPredicate<T, ValidationGroup[]> {
 
 }
